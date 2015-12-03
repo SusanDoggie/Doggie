c688fd6268d234c89a48c9f85b6da75dab12cf3b
SusanDoggie <susan.doggie@gmail.com>
2015-12-03 11:12:50 +0800

    cubic equation decompose to linear equation and quad equation

--- Doggie/Maths/Maths.swift
+++ Doggie/Maths/Maths.swift
@@ -227,6 +227,40 @@ public func degree2roots(b: Double, _ c: Double) -> [Double] {
     return []
 }
 
+public func degree3decompose(b: Double, _ c: Double, _ d: Double) -> (Double, (Double, Double)) {
+    if d.almostZero {
+        return (0, (b, c))
+    }
+    let b2 = b * b
+    let b3 = b * b2
+    let de0 = b2 - 3 * c
+    let de1 = 2 * b3 - 9 * c * b + 27 * d
+    let de2 = de1 * de1 - 4 * de0 * de0 * de0
+    
+    if de2.isSignMinus { // delta > 0, three real roots
+        let m = b / 3
+        let p = -de0 / 3
+        let q = de1 / 27
+        let p_3 = p / 3
+        let s = 2 * sqrt(-p_3)
+        let t = acos(q / (p_3 * s)) / 3
+        let u = M_PI * 2 / 3
+        let cos1 = cos(t)
+        let cos2 = cos(t - u)
+        let cos3 = cos(t - 2 * u)
+        let s_cos1 = s * cos1
+        let s_cos2 = s * cos2
+        let s_cos3 = s * cos3
+        let k = m - s_cos1 * cos2
+        return (s_cos2 - m, (2 * k, s_cos1 * s_cos3 + m * k))
+    }
+    
+    let c1 = cbrt(0.5 * (de1 + sqrt(de2)))
+    let c2 = cbrt(0.5 * (de1 - sqrt(de2)))
+    let c3 = c1 + c2
+    return ((-b - c3) / 3, ((2 * b - c3) / 3, (b2 - b * c3 + c3 * c3 - 3 * c1 * c2) / 9))
+}
+
 public func degree3roots(b: Double, _ c: Double, _ d: Double) -> [Double] {
     if d.almostZero {
         let z = degree2roots(b, c)
