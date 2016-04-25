8f8fe64e54c7646164d80a836d7088b625c3e867
SusanDoggie <susan.doggie@gmail.com>
2016-04-25 16:58:42 +0800

    clean

--- Doggie/Maths/SDTransform.swift
+++ Doggie/Maths/SDTransform.swift
@@ -153,9 +153,9 @@ extension SDTransform {
     ///
     /// Transformation Matrix:
     ///
-    ///     ⎛ cos(a) sin(a) 0 ⎞
-    ///     ⎜ -sin(a)  cos(a) 0 ⎟
-    ///     ⎝   0       0    1 ⎠
+    ///     ⎛  cos(a) sin(a) 0 ⎞
+    ///     ⎜ -sin(a) cos(a) 0 ⎟
+    ///     ⎝    0      0    1 ⎠
     ///
     public struct Rotate: SDTransformType {
         
