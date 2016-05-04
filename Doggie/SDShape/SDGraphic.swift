c1d3b2ee111d686b84f924a270989f13588658b9
SusanDoggie <susan.doggie@gmail.com>
2016-05-04 12:43:40 +0800

    improve

--- Doggie/SDShape/SDGraphic.swift
+++ Doggie/SDShape/SDGraphic.swift
@@ -134,26 +134,11 @@ extension SDPath {
         let path = CGPathCreateMutable()
         self.apply { component, state in
             switch component {
-            case let move as SDPath.Move:
-                
-                CGPathMoveToPoint(path, nil, CGFloat(move.x), CGFloat(move.y))
-                
-            case let line as SDPath.Line:
-                
-                CGPathAddLineToPoint(path, nil, CGFloat(line.x), CGFloat(line.y))
-                
-            case let quad as SDPath.QuadBezier:
-                
-                CGPathAddQuadCurveToPoint(path, nil, CGFloat(quad.p1.x), CGFloat(quad.p1.y), CGFloat(quad.p2.x), CGFloat(quad.p2.y))
-                
-            case let cubic as SDPath.CubicBezier:
-                
-                CGPathAddCurveToPoint(path, nil, CGFloat(cubic.p1.x), CGFloat(cubic.p1.y), CGFloat(cubic.p2.x), CGFloat(cubic.p2.y), CGFloat(cubic.p3.x), CGFloat(cubic.p3.y))
-                
-            case _ as SDPath.ClosePath:
-                
-                CGPathCloseSubpath(path)
-                
+            case let move as SDPath.Move: CGPathMoveToPoint(path, nil, CGFloat(move.x), CGFloat(move.y))
+            case let line as SDPath.Line: CGPathAddLineToPoint(path, nil, CGFloat(line.x), CGFloat(line.y))
+            case let quad as SDPath.QuadBezier: CGPathAddQuadCurveToPoint(path, nil, CGFloat(quad.p1.x), CGFloat(quad.p1.y), CGFloat(quad.p2.x), CGFloat(quad.p2.y))
+            case let cubic as SDPath.CubicBezier: CGPathAddCurveToPoint(path, nil, CGFloat(cubic.p1.x), CGFloat(cubic.p1.y), CGFloat(cubic.p2.x), CGFloat(cubic.p2.y), CGFloat(cubic.p3.x), CGFloat(cubic.p3.y))
+            case _ as SDPath.ClosePath: CGPathCloseSubpath(path)
             default: break
             }
         }
@@ -167,18 +152,15 @@ extension SDPath {
     public init(_ path: CoreGraphics.CGPath) {
         self.init()
         CGPathApply(path, &self) { buf, element in
-            
             let path = UnsafeMutablePointer<SDPath>(buf)
             let points = element.memory.points
-            
             switch element.memory.type {
-            case .MoveToPoint: path.memory.append(SDPath.Move(Point(points[0])))
-            case .AddLineToPoint: path.memory.append(SDPath.Line(Point(points[0])))
-            case .AddQuadCurveToPoint: path.memory.append(SDPath.QuadBezier(Point(points[0]), Point(points[1])))
-            case .AddCurveToPoint: path.memory.append(SDPath.CubicBezier(Point(points[0]), Point(points[1]), Point(points[2])))
-            case .CloseSubpath: path.memory.append(SDPath.ClosePath())
+            case .MoveToPoint: path.memory.appendCommand(SDPath.Move(Point(points[0])))
+            case .AddLineToPoint: path.memory.appendCommand(SDPath.Line(Point(points[0])))
+            case .AddQuadCurveToPoint: path.memory.appendCommand(SDPath.QuadBezier(Point(points[0]), Point(points[1])))
+            case .AddCurveToPoint: path.memory.appendCommand(SDPath.CubicBezier(Point(points[0]), Point(points[1]), Point(points[2])))
+            case .CloseSubpath: path.memory.appendCommand(SDPath.ClosePath())
             }
-            
         }
     }
 }
