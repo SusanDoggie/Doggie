11545412cd4c3cd348fd977ae82c3669d90f6f4e
SusanDoggie <susan.doggie@gmail.com>
2016-10-01 17:27:06 +0800

    lock free queue

---
+++ Sources/AtomicQueue.swift
@@ -0,0 +1,72 @@
+//
+//  AtomicQueue.swift
+//
+//  The MIT License
+//  Copyright (c) 2015 - 2016 Susan Cheng. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+private class AtomicQueueContainer<Instance> {
+    
+    let value: Instance!
+    var next: Atomic<AtomicQueueContainer<Instance>?>
+    
+    init(value: Instance! = nil, next: AtomicQueueContainer<Instance>? = nil) {
+        self.value = value
+        self.next = Atomic(value: next)
+    }
+}
+
+public class AtomicQueue<Instance> {
+    
+    fileprivate var head: Atomic<AtomicQueueContainer<Instance>>
+    fileprivate var tail: AtomicQueueContainer<Instance>
+    
+    public init() {
+        let telomere = AtomicQueueContainer<Instance>()
+        self.head = Atomic(value: telomere)
+        self.tail = telomere
+    }
+    
+    public func enqueue(_ newElement: Instance) {
+        let new = AtomicQueueContainer(value: newElement, next: nil)
+        while true {
+            let _tail = tail.next.fetchSelf()
+            if _tail.value != nil {
+                tail = _tail.value!
+            } else if tail.next.compareSet(old: _tail.current, new: new) {
+                return
+            }
+        }
+    }
+    
+    public func dequeue() -> Instance? {
+        while let _tail = tail.next.fetch() {
+            tail = _tail
+        }
+        while true {
+            let _head = head.fetchSelf()
+            let _next = _head.value.next.fetchSelf()
+            if head.compareSet(old: _head.current, new: _next.value ?? _head.value) {
+                return _next.value?.value
+            }
+        }
+    }
+}
