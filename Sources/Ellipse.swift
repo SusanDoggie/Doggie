336e4632e2bc99093db0f3b1c30b6ca901e6be8e
SusanDoggie <susan.doggie@gmail.com>
2016-10-31 23:06:02 +0800

    clean

---
+++ Sources/Ellipse.swift
@@ -0,0 +1,160 @@
+//
+//  Ellipse.swift
+//
+//  The MIT License
+//  Copyright (c) 2015 - 2016 Susan Cheng. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+
+public struct Radius {
+    
+    public var x: Double
+    public var y: Double
+    
+    public init() {
+        self.x = 0
+        self.y = 0
+    }
+    
+    public init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+    public init(x: Int, y: Int) {
+        self.x = Double(x)
+        self.y = Double(y)
+    }
+}
+
+extension Radius: CustomStringConvertible {
+    
+    public var description: String {
+        return "{x: \(x), y: \(y)}"
+    }
+}
+
+extension Radius: Hashable {
+    
+    public var hashValue: Int {
+        return hash_combine(seed: 0, x, y)
+    }
+}
+
+public func == (lhs: Radius, rhs: Radius) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y
+}
+public func != (lhs: Radius, rhs: Radius) -> Bool {
+    return lhs.x != rhs.x || lhs.y != rhs.y
+}
+
+public func Ellipse(_ t: Double, _ p: Point, _ r: Radius) -> Point {
+    return Point(x: r.x * cos(t) + p.x, y: r.y * sin(t) + p.y)
+}
+
+public func EllipseRadius(_ p0: Point, _ p1: Point, _ r: Radius, _ rotate: Double) -> Radius {
+    let _p = p1 - p0
+    let _tx = _p.x * cos(rotate) + _p.y * sin(rotate)
+    let _ty = _p.y * cos(rotate) - _p.x * sin(rotate)
+    let _atan = atan2(_ty / r.y, _tx / r.x)
+    return Radius(x: _tx / (2 * cos(_atan)), y: _ty / (2 * sin(_atan)))
+}
+
+public func EllipseCenter(_ r: Radius, _ rotate: Double, _ a: Point, _ b: Point) -> [Point] {
+    
+    let _sin = sin(rotate)
+    let _cos = cos(rotate)
+    
+    let ax = a.x * _cos + a.y * _sin
+    let ay = a.y * _cos - a.x * _sin
+    let bx = b.x * _cos + b.y * _sin
+    let by = b.y * _cos - b.x * _sin
+    
+    let dx = (ax - bx) / r.x
+    let dy = (ay - by) / r.y
+    let d = dx * dx + dy * dy
+    
+    let _x = 0.5 * (ax + bx)
+    let _y = 0.5 * (ay + by)
+    
+    if d == 4 {
+        return [Point(x: _x * _cos - _y * _sin, y: _x * _sin + _y * _cos)]
+    } else if d < 4 {
+        let _t = sqrt((1 - d * 0.25) / d)
+        
+        let cx1 = _x + _t * dy * r.x
+        let cy1 = _y - _t * dx * r.y
+        let cx2 = _x - _t * dy * r.x
+        let cy2 = _y + _t * dx * r.y
+        
+        return [Point(x: cx1 * _cos - cy1 * _sin, y: cx1 * _sin + cy1 * _cos),
+                Point(x: cx2 * _cos - cy2 * _sin, y: cx2 * _sin + cy2 * _cos)]
+    }
+    
+    return []
+}
+
+///
+/// :param: rx radius of ellipse in x-axis
+/// :param: ry radius of ellipse in y-axis
+///
+/// :param: a value of 'a' in matrix if parallel to x-axis or value of 'd' in matrix if parallel to y-axis.
+/// :param: b value of 'b' in matrix if parallel to x-axis or value of 'e' in matrix if parallel to y-axis.
+///
+/// Transformation Matrix:
+///
+///     ⎛ a b c ⎞ ⎛ A cos(t) ⎞
+///     ⎜ d e f ⎟ ⎜ B sin(t) ⎟
+///     ⎝ 0 0 1 ⎠ ⎝    1     ⎠
+///
+public func EllipseStationary(_ r: Radius, _ a: Double, _ b: Double) -> Double {
+    return atan2(r.y * b, r.x * a)
+}
+
+///
+/// Transformation Matrix:
+///
+///     ⎛ a b c ⎞ ⎛ A cos(t) ⎞
+///     ⎜ d e f ⎟ ⎜ B sin(t) ⎟
+///     ⎝ 0 0 1 ⎠ ⎝    1     ⎠
+///
+public func EllipseBound<T: SDTransformProtocol>(_ center: Point, _ r: Radius, _ matrix: T) -> Rect {
+    
+    let t1 = EllipseStationary(r, matrix.a, matrix.b)
+    let t2 = EllipseStationary(r, matrix.d, matrix.e)
+    
+    let p0 = Ellipse(t1, center, r)
+    let p1 = Ellipse(t1 + M_PI, center, r)
+    let p2 = Ellipse(t2, center, r)
+    let p3 = Ellipse(t2 + M_PI, center, r)
+    
+    let _p0 = matrix.a * p0.x + matrix.b * p0.y
+    let _p1 = matrix.a * p1.x + matrix.b * p1.y
+    let _p2 = matrix.d * p2.x + matrix.e * p2.y
+    let _p3 = matrix.d * p3.x + matrix.e * p3.y
+    
+    let minX = min(_p0, _p1)
+    let minY = min(_p2, _p3)
+    let maxX = max(_p0, _p1)
+    let maxY = max(_p2, _p3)
+    
+    return Rect(x: minX + matrix.c, y: minY + matrix.f, width: maxX - minX, height: maxY - minY)
+}
