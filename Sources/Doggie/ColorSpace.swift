4379a6b4aae5a5586475b058d3a1f8c6af3b8e7e
Susan <CloudDesign@Cloud-Design-IT.local>
2017-01-13 10:59:26 +0800

    bug fix

--- Sources/Doggie/ColorSpace.swift
+++ Sources/Doggie/ColorSpace.swift
@@ -293,8 +293,8 @@ extension CIELuvColorSpace {
         let st = 216.0 / 27.0
         let _white = XYZColorModel(luminance: 1, x: white.x, y: white.y)
         let n = 1 / (_white.x + 15 * _white.y + 3 * _white.z)
-        let _uw = 4 * white.x * n
-        let _vw = 9 * white.y * n
+        let _uw = 4 * _white.x * n
+        let _vw = 9 * _white.y * n
         let fy = (color.lightness + 16) / 116
         let y = color.lightness > st ? fy * fy * fy : t * color.lightness
         let a = 52 * color.lightness / (color.u + 13 * color.lightness * _uw) - 1
@@ -310,11 +310,11 @@ extension CIELuvColorSpace {
         let _white = XYZColorModel(luminance: 1, x: white.x, y: white.y)
         let m = 1 / (color.x + 15 * color.y + 3 * color.z)
         let n = 1 / (_white.x + 15 * _white.y + 3 * _white.z)
-        let y = color.y / white.y
+        let y = color.y / _white.y
         let _u = 4 * color.x * m
         let _v = 9 * color.y * m
-        let _uw = 4 * white.x * n
-        let _vw = 9 * white.y * n
+        let _uw = 4 * _white.x * n
+        let _vw = 9 * _white.y * n
         let l = y > s ? 116 * cbrt(y) - 16 : t * y
         return LuvColorModel(lightness: l, u: 13 * l * (_u - _uw), v: 13 * l * (_v - _vw))
     }
