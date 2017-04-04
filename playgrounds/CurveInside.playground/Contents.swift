ab5c960c4470d45d3568e3a2c5c7d1312222ce6c
Susan Cheng <Susan@SusandeMac-mini.local>
2017-04-04 14:32:51 +0800

    test

---
+++ playgrounds/CurveInside.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import Doggie
+import PlaygroundSupport
+
+PlaygroundPage.current.liveView = QuadCurveInsideView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
