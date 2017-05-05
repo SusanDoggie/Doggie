b8fad29fc205e07d4f56495f5cbdd8a7c4bfd6bb
Susan <CloudDesign@Cloud-Design-IT.local>
2017-05-05 11:43:36 +0800

    clean

--- Playground/Color.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Playground/Color.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -3,12 +3,12 @@
 import Cocoa
 import Doggie
 
-let srgb = CalibratedRGBColorSpace.sRGB
-
 let size = 500
 
 let range: Double = 128
 
-showLab(size: size, x: -range...range, y: -range...range, z: 50, colorSpace: srgb)
+showLab(size: size, x: -range...range, y: -range...range, z: 50, colorSpace: CalibratedRGBColorSpace.sRGB)
 
-showLuv(size: size, x: -range...range, y: -range...range, z: 50, colorSpace: srgb)
+showLab(size: size, x: -range...range, y: -range...range, z: 50, colorSpace: CalibratedRGBColorSpace.adobeRGB)
+
+showLab(size: size, x: -range...range, y: -range...range, z: 50, colorSpace: CalibratedRGBColorSpace.displayP3)
