8f44de09957a09c6a2e14382b520f058759b548a
Susan <CloudDesign@Cloud-Design-IT.local>
2017-06-09 12:32:33 +0800

    clean

--- Playground/Resampling.playground/Contents.swift
+++ Playground/Resampling.playground/Contents.swift
@@ -3,93 +3,28 @@
 import Cocoa
 import Doggie
 
-let _colorspace = CGColorSpace(name: CGColorSpace.linearSRGB) ?? CGColorSpaceCreateDeviceRGB()
-let _bitmapInfo = CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue
+let context = ImageContext(width: 100, height: 100, colorSpace: CalibratedRGBColorSpace.sRGB)
 
-public func createImage(data rawData: UnsafeRawPointer, size: CGSize) -> CGImage? {
-    
-    let imageWidth = Int(size.width)
-    let imageHeight = Int(size.height)
-    
-    let bitsPerComponent: Int = 8
-    let bytesPerPixel: Int = 4
-    let bitsPerPixel: Int = bytesPerPixel * bitsPerComponent
-    
-    let bytesPerRow = bytesPerPixel * imageWidth
-    
-    return CGImage.create(rawData, width: imageWidth, height: imageHeight, bitsPerComponent: bitsPerComponent, bitsPerPixel: bitsPerPixel, bytesPerRow: bytesPerRow, space: _colorspace, bitmapInfo: _bitmapInfo)
-}
+context.draw(shape: Shape.Ellipse(Rect(x: 10, y: 35, width: 55, height: 55)), color: Color(colorSpace: CalibratedRGBColorSpace.sRGB, color: RGBColorModel(red: 247/255, green: 217/255, blue: 12/255)), winding: .nonZero)
 
-extension NSImage {
-    
-    public convenience init(cgImage image: CGImage) {
-        self.init(cgImage: image, size: NSZeroSize)
-    }
-}
+context.draw(shape: Shape.Ellipse(Rect(x: 10, y: 35, width: 55, height: 55)).strokePath(width: 1, cap: .round, join: .round), color: Color(colorSpace: CalibratedRGBColorSpace.sRGB, color: RGBColorModel()), winding: .nonZero)
 
-var sample = Image(width: 100, height: 100, colorSpace: CalibratedRGBColorSpace.sRGB.linearTone, pixel: ARGB32ColorPixel())
+context.draw(shape: Shape.Ellipse(Rect(x: 35, y: 10, width: 55, height: 55)), color: Color(colorSpace: CalibratedRGBColorSpace.sRGB, color: RGBColorModel(red: 234/255, green: 24/255, blue: 71/255)), winding: .nonZero)
 
-sample.withUnsafeMutableBufferPointer {
-    if let context = CGContext(data: $0.baseAddress!, width: 100, height: 100, bitsPerComponent: 8, bytesPerRow: 400, space: _colorspace, bitmapInfo: _bitmapInfo) {
-        
-        context.setStrokeColor(NSColor.black.cgColor)
-        context.setFillColor(NSColor(calibratedRed: 247/255, green: 217/255, blue: 12/255, alpha: 1).cgColor)
-        
-        context.fillEllipse(in: CGRect(x: 10, y: 35, width: 55, height: 55))
-        context.strokeEllipse(in: CGRect(x: 10, y: 35, width: 55, height: 55))
-        
-        context.setFillColor(NSColor(calibratedRed: 234/255, green: 24/255, blue: 71/255, alpha: 1).cgColor)
-        
-        context.fillEllipse(in: CGRect(x: 35, y: 10, width: 55, height: 55))
-        context.strokeEllipse(in: CGRect(x: 35, y: 10, width: 55, height: 55))
-        
-    }
-}
+context.draw(shape: Shape.Ellipse(Rect(x: 35, y: 10, width: 55, height: 55)).strokePath(width: 1, cap: .round, join: .round), color: Color(colorSpace: CalibratedRGBColorSpace.sRGB, color: RGBColorModel()), winding: .nonZero)
 
-sample.withUnsafeBufferPointer {
-    if let image = createImage(data: $0.baseAddress!, size: CGSize(width: 100, height: 100)) {
-        NSImage(cgImage: image)
-    }
-}
+let sample = Image<ARGB32ColorPixel>(image: context.image)
 
-Image(image: sample, width: 1000, height: 1000, resampling: .none).withUnsafeBufferPointer {
-    if let image = createImage(data: $0.baseAddress!, size: CGSize(width: 1000, height: 1000)) {
-        NSImage(cgImage: image)
-    }
-}
+Image(image: sample, width: 1000, height: 1000, resampling: .none).cgImage
 
-Image(image: sample, width: 1000, height: 1000, resampling: .linear).withUnsafeBufferPointer {
-    if let image = createImage(data: $0.baseAddress!, size: CGSize(width: 1000, height: 1000)) {
-        NSImage(cgImage: image)
-    }
-}
+Image(image: sample, width: 1000, height: 1000, resampling: .linear).cgImage
 
-Image(image: sample, width: 1000, height: 1000, resampling: .cosine).withUnsafeBufferPointer {
-    if let image = createImage(data: $0.baseAddress!, size: CGSize(width: 1000, height: 1000)) {
-        NSImage(cgImage: image)
-    }
-}
+Image(image: sample, width: 1000, height: 1000, resampling: .cosine).cgImage
 
-Image(image: sample, width: 1000, height: 1000, resampling: .cubic).withUnsafeBufferPointer {
-    if let image = createImage(data: $0.baseAddress!, size: CGSize(width: 1000, height: 1000)) {
-        NSImage(cgImage: image)
-    }
-}
+Image(image: sample, width: 1000, height: 1000, resampling: .cubic).cgImage
 
-Image(image: sample, width: 1000, height: 1000, resampling: .hermite(0.5, 0)).withUnsafeBufferPointer {
-    if let image = createImage(data: $0.baseAddress!, size: CGSize(width: 1000, height: 1000)) {
-        NSImage(cgImage: image)
-    }
-}
+Image(image: sample, width: 1000, height: 1000, resampling: .hermite(0.5, 0)).cgImage
 
-Image(image: sample, width: 1000, height: 1000, resampling: .mitchell(1/3, 1/3)).withUnsafeBufferPointer {
-    if let image = createImage(data: $0.baseAddress!, size: CGSize(width: 1000, height: 1000)) {
-        NSImage(cgImage: image)
-    }
-}
+Image(image: sample, width: 1000, height: 1000, resampling: .mitchell(1/3, 1/3)).cgImage
 
-Image(image: sample, width: 1000, height: 1000, resampling: .lanczos(3)).withUnsafeBufferPointer {
-    if let image = createImage(data: $0.baseAddress!, size: CGSize(width: 1000, height: 1000)) {
-        NSImage(cgImage: image)
-    }
-}
+Image(image: sample, width: 1000, height: 1000, resampling: .lanczos(3)).cgImage
