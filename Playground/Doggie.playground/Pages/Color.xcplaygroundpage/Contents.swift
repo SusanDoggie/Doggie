4d6d798d2822b46e943ddb236fe1b07ae6dad4db
Susan <CloudDesign@Cloud-Design-IT.local>
2017-06-12 11:31:09 +0800

    clean

--- Playground/Doggie.playground/Pages/Color.xcplaygroundpage/Contents.swift
+++ Playground/Doggie.playground/Pages/Color.xcplaygroundpage/Contents.swift
@@ -3,6 +3,16 @@
 import Cocoa
 import Doggie
 
+let D50 = ColorSpace.cieXYZ(white: XYZColorModel(luminance: 1, x: 0.34567, y: 0.35850))
+
+ColorSpace.adobeRGB.convert(RGBColorModel(red: 0.5, green: 0, blue: 0), to: D50)
+
+ColorSpace.adobeRGB.normalized.convert(RGBColorModel(red: 0.5, green: 0, blue: 0), to: D50)
+
+ColorSpace.adobeRGB.convert(RGBColorModel(red: 0.5, green: 0, blue: 0), to: ColorSpace.adobeRGB.linearTone)
+
+ColorSpace.adobeRGB.linearTone.convert(RGBColorModel(red: 0.217755528144395, green: 0, blue: 0), to: ColorSpace.adobeRGB)
+
 do {
     
     let cgColor1 = NSColor(colorSpace: NSColorSpace.adobeRGB1998, components: [0.5, 0, 0, 1], count: 4).cgColor
