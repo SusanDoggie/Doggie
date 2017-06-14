35607c10a153a0ecd82e13106e8eae9d46372195
Susan <CloudDesign@Cloud-Design-IT.local>
2017-06-14 18:34:10 +0800

    clean

--- Sources/Doggie/Color/ColorSpace/iccProfile/iccTag.swift
+++ Sources/Doggie/Color/ColorSpace/iccProfile/iccTag.swift
@@ -61,71 +61,71 @@ extension iccProfile {
         
         case unknown
         
-        case AToB0Tag                          = 0x41324230  /* 'A2B0' */
-        case AToB1Tag                          = 0x41324231  /* 'A2B1' */
-        case AToB2Tag                          = 0x41324232  /* 'A2B2' */
-        case BlueColorantTag                   = 0x6258595A  /* 'bXYZ' */
-        case BlueTRCTag                        = 0x62545243  /* 'bTRC' */
-        case BToA0Tag                          = 0x42324130  /* 'B2A0' */
-        case BToA1Tag                          = 0x42324131  /* 'B2A1' */
-        case BToA2Tag                          = 0x42324132  /* 'B2A2' */
-        case CalibrationDateTimeTag            = 0x63616C74  /* 'calt' */
-        case CharTargetTag                     = 0x74617267  /* 'targ' */
-        case ChromaticAdaptationTag            = 0x63686164  /* 'chad' */
-        case ChromaticityTag                   = 0x6368726D  /* 'chrm' */
-        case ColorantOrderTag                  = 0x636C726F  /* 'clro' */
-        case ColorantTableTag                  = 0x636C7274  /* 'clrt' */
-        case ColorantTableOutTag               = 0x636C6F74  /* 'clot' */
+        case AToB0                          = 0x41324230  /* 'A2B0' */
+        case AToB1                          = 0x41324231  /* 'A2B1' */
+        case AToB2                          = 0x41324232  /* 'A2B2' */
+        case BlueColorant                   = 0x6258595A  /* 'bXYZ' */
+        case BlueTRC                        = 0x62545243  /* 'bTRC' */
+        case BToA0                          = 0x42324130  /* 'B2A0' */
+        case BToA1                          = 0x42324131  /* 'B2A1' */
+        case BToA2                          = 0x42324132  /* 'B2A2' */
+        case CalibrationDateTime            = 0x63616C74  /* 'calt' */
+        case CharTarget                     = 0x74617267  /* 'targ' */
+        case ChromaticAdaptation            = 0x63686164  /* 'chad' */
+        case Chromaticity                   = 0x6368726D  /* 'chrm' */
+        case ColorantOrder                  = 0x636C726F  /* 'clro' */
+        case ColorantTable                  = 0x636C7274  /* 'clrt' */
+        case ColorantTableOut               = 0x636C6F74  /* 'clot' */
         case ColorimetricIntentImageStateTag   = 0x63696973  /* 'ciis' */
-        case CopyrightTag                      = 0x63707274  /* 'cprt' */
-        case CrdInfoTag                        = 0x63726469  /* 'crdi' Removed in V4 */
-        case DataTag                           = 0x64617461  /* 'data' Removed in V4 */
-        case DateTimeTag                       = 0x6474696D  /* 'dtim' Removed in V4 */
-        case DeviceMfgDescTag                  = 0x646D6E64  /* 'dmnd' */
-        case DeviceModelDescTag                = 0x646D6464  /* 'dmdd' */
-        case DeviceSettingsTag                 = 0x64657673  /* 'devs' Removed in V4 */
-        case DToB0Tag                          = 0x44324230  /* 'D2B0' */
-        case DToB1Tag                          = 0x44324231  /* 'D2B1' */
-        case DToB2Tag                          = 0x44324232  /* 'D2B2' */
-        case DToB3Tag                          = 0x44324233  /* 'D2B3' */
-        case BToD0Tag                          = 0x42324430  /* 'B2D0' */
-        case BToD1Tag                          = 0x42324431  /* 'B2D1' */
-        case BToD2Tag                          = 0x42324432  /* 'B2D2' */
-        case BToD3Tag                          = 0x42324433  /* 'B2D3' */
-        case GamutTag                          = 0x67616D74  /* 'gamt' */
-        case GrayTRCTag                        = 0x6b545243  /* 'kTRC' */
-        case GreenColorantTag                  = 0x6758595A  /* 'gXYZ' */
-        case GreenTRCTag                       = 0x67545243  /* 'gTRC' */
-        case LuminanceTag                      = 0x6C756d69  /* 'lumi' */
-        case MeasurementTag                    = 0x6D656173  /* 'meas' */
-        case MediaBlackPointTag                = 0x626B7074  /* 'bkpt' */
-        case MediaWhitePointTag                = 0x77747074  /* 'wtpt' */
-        case MetaDataTag                       = 0x6D657461  /* 'meta' */
-        case NamedColor2Tag                    = 0x6E636C32  /* 'ncl2' */
-        case OutputResponseTag                 = 0x72657370  /* 'resp' */
+        case Copyright                      = 0x63707274  /* 'cprt' */
+        case CrdInfo                        = 0x63726469  /* 'crdi' Removed in V4 */
+        case Data                           = 0x64617461  /* 'data' Removed in V4 */
+        case DateTime                       = 0x6474696D  /* 'dtim' Removed in V4 */
+        case DeviceMfgDesc                  = 0x646D6E64  /* 'dmnd' */
+        case DeviceModelDesc                = 0x646D6464  /* 'dmdd' */
+        case DeviceSettings                 = 0x64657673  /* 'devs' Removed in V4 */
+        case DToB0                          = 0x44324230  /* 'D2B0' */
+        case DToB1                          = 0x44324231  /* 'D2B1' */
+        case DToB2                          = 0x44324232  /* 'D2B2' */
+        case DToB3                          = 0x44324233  /* 'D2B3' */
+        case BToD0                          = 0x42324430  /* 'B2D0' */
+        case BToD1                          = 0x42324431  /* 'B2D1' */
+        case BToD2                          = 0x42324432  /* 'B2D2' */
+        case BToD3                          = 0x42324433  /* 'B2D3' */
+        case Gamut                          = 0x67616D74  /* 'gamt' */
+        case GrayTRC                        = 0x6b545243  /* 'kTRC' */
+        case GreenColorant                  = 0x6758595A  /* 'gXYZ' */
+        case GreenTRC                       = 0x67545243  /* 'gTRC' */
+        case Luminance                      = 0x6C756d69  /* 'lumi' */
+        case Measurement                    = 0x6D656173  /* 'meas' */
+        case MediaBlackPoint                = 0x626B7074  /* 'bkpt' */
+        case MediaWhitePoint                = 0x77747074  /* 'wtpt' */
+        case MetaData                       = 0x6D657461  /* 'meta' */
+        case NamedColor2                    = 0x6E636C32  /* 'ncl2' */
+        case OutputResponse                 = 0x72657370  /* 'resp' */
         case PerceptualRenderingIntentGamutTag = 0x72696730  /* 'rig0' */
-        case Preview0Tag                       = 0x70726530  /* 'pre0' */
-        case Preview1Tag                       = 0x70726531  /* 'pre1' */
-        case Preview2Tag                       = 0x70726532  /* 'pre2' */
-        case PrintConditionTag                 = 0x7074636e  /* 'ptcn' */
-        case ProfileDescriptionTag             = 0x64657363  /* 'desc' */
-        case ProfileSequenceDescTag            = 0x70736571  /* 'pseq' */
-        case ProfileSequceIdTag                = 0x70736964  /* 'psid' */
-        case Ps2CRD0Tag                        = 0x70736430  /* 'psd0' Removed in V4 */
-        case Ps2CRD1Tag                        = 0x70736431  /* 'psd1' Removed in V4 */
-        case Ps2CRD2Tag                        = 0x70736432  /* 'psd2' Removed in V4 */
-        case Ps2CRD3Tag                        = 0x70736433  /* 'psd3' Removed in V4 */
-        case Ps2CSATag                         = 0x70733273  /* 'ps2s' Removed in V4 */
-        case Ps2RenderingIntentTag             = 0x70733269  /* 'ps2i' Removed in V4 */
-        case RedColorantTag                    = 0x7258595A  /* 'rXYZ' */
-        case RedTRCTag                         = 0x72545243  /* 'rTRC' */
+        case Preview0                       = 0x70726530  /* 'pre0' */
+        case Preview1                       = 0x70726531  /* 'pre1' */
+        case Preview2                       = 0x70726532  /* 'pre2' */
+        case PrintCondition                 = 0x7074636e  /* 'ptcn' */
+        case ProfileDescription             = 0x64657363  /* 'desc' */
+        case ProfileSequenceDesc            = 0x70736571  /* 'pseq' */
+        case ProfileSequceId                = 0x70736964  /* 'psid' */
+        case Ps2CRD0                        = 0x70736430  /* 'psd0' Removed in V4 */
+        case Ps2CRD1                        = 0x70736431  /* 'psd1' Removed in V4 */
+        case Ps2CRD2                        = 0x70736432  /* 'psd2' Removed in V4 */
+        case Ps2CRD3                        = 0x70736433  /* 'psd3' Removed in V4 */
+        case Ps2CSA                         = 0x70733273  /* 'ps2s' Removed in V4 */
+        case Ps2RenderingIntent             = 0x70733269  /* 'ps2i' Removed in V4 */
+        case RedColorant                    = 0x7258595A  /* 'rXYZ' */
+        case RedTRC                         = 0x72545243  /* 'rTRC' */
         case SaturationRenderingIntentGamutTag = 0x72696732  /* 'rig2' */
-        case ScreeningDescTag                  = 0x73637264  /* 'scrd' Removed in V4 */
-        case ScreeningTag                      = 0x7363726E  /* 'scrn' Removed in V4 */
-        case TechnologyTag                     = 0x74656368  /* 'tech' */
-        case UcrBgTag                          = 0x62666420  /* 'bfd ' Removed in V4 */
-        case ViewingCondDescTag                = 0x76756564  /* 'vued' */
-        case ViewingConditionsTag              = 0x76696577  /* 'view' */
+        case ScreeningDesc                  = 0x73637264  /* 'scrd' Removed in V4 */
+        case Screening                      = 0x7363726E  /* 'scrn' Removed in V4 */
+        case Technology                     = 0x74656368  /* 'tech' */
+        case UcrBg                          = 0x62666420  /* 'bfd ' Removed in V4 */
+        case ViewingCondDesc                = 0x76756564  /* 'vued' */
+        case ViewingConditions              = 0x76696577  /* 'view' */
     }
 }
 
@@ -167,13 +167,13 @@ extension iccProfile.TagData : CustomStringConvertible {
     public var description: String {
         
         switch type {
-        case .UInt16ArrayType: return "\(uInt16Array ?? []))"
-        case .UInt32ArrayType: return "\(uInt32Array ?? []))"
-        case .UInt64ArrayType: return "\(uInt64Array ?? []))"
-        case .UInt8ArrayType: return "\(uInt8Array ?? []))"
-        case .S15Fixed16ArrayType: return "\(s15Fixed16Array ?? []))"
-        case .U16Fixed16ArrayType: return "\(u16Fixed16Array ?? []))"
-        case .XYZArrayType: return "\(XYZArray ?? []))"
+        case .UInt16Array: return "\(uInt16Array ?? []))"
+        case .UInt32Array: return "\(uInt32Array ?? []))"
+        case .UInt64Array: return "\(uInt64Array ?? []))"
+        case .UInt8Array: return "\(uInt8Array ?? []))"
+        case .S15Fixed16Array: return "\(s15Fixed16Array ?? []))"
+        case .U16Fixed16Array: return "\(u16Fixed16Array ?? []))"
+        case .XYZArray: return "\(XYZArray ?? []))"
         default: return "TagData(type:\(type), data:\(data))"
         }
     }
@@ -183,22 +183,22 @@ extension iccProfile.TagData {
     
     public var uInt16Array: [iccProfile.UInt16Number]? {
         
-        return type == .UInt16ArrayType ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.UInt16Number>.stride)) } : nil
+        return type == .UInt16Array ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.UInt16Number>.stride)) } : nil
     }
     
     public var uInt32Array: [iccProfile.UInt32Number]? {
         
-        return type == .UInt32ArrayType ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.UInt32Number>.stride)) } : nil
+        return type == .UInt32Array ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.UInt32Number>.stride)) } : nil
     }
     
     public var uInt64Array: [iccProfile.UInt64Number]? {
         
-        return type == .UInt64ArrayType ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.UInt64Number>.stride)) } : nil
+        return type == .UInt64Array ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.UInt64Number>.stride)) } : nil
     }
     
     public var uInt8Array: [UInt8]? {
         
-        return type == .UInt8ArrayType ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<UInt8>.stride)) } : nil
+        return type == .UInt8Array ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<UInt8>.stride)) } : nil
     }
 }
 
@@ -206,12 +206,12 @@ extension iccProfile.TagData {
     
     public var s15Fixed16Array: [iccProfile.S15Fixed16Number]? {
         
-        return type == .S15Fixed16ArrayType ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.S15Fixed16Number>.stride)) } : nil
+        return type == .S15Fixed16Array ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.S15Fixed16Number>.stride)) } : nil
     }
     
     public var u16Fixed16Array: [iccProfile.U16Fixed16Number]? {
         
-        return type == .U16Fixed16ArrayType ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.U16Fixed16Number>.stride)) } : nil
+        return type == .U16Fixed16Array ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.U16Fixed16Number>.stride)) } : nil
     }
 }
 
@@ -219,7 +219,7 @@ extension iccProfile.TagData {
     
     public var XYZArray: [iccProfile.XYZNumber]? {
         
-        return type == .XYZArrayType ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.XYZNumber>.stride)) } : nil
+        return type == .XYZArray ? data.withUnsafeBytes { Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<iccProfile.XYZNumber>.stride)) } : nil
     }
 }
 
@@ -229,39 +229,39 @@ extension iccProfile.TagData {
         
         case unknown
         
-        case ChromaticityType               = 0x6368726D  /* 'chrm' */
-        case ColorantOrderType              = 0x636C726F  /* 'clro' */
-        case ColorantTableType              = 0x636C7274  /* 'clrt' */
-        case CrdInfoType                    = 0x63726469  /* 'crdi' Removed in V4 */
-        case CurveType                      = 0x63757276  /* 'curv' */
-        case DataType                       = 0x64617461  /* 'data' */
-        case DictType                       = 0x64696374  /* 'dict' */
-        case DateTimeType                   = 0x6474696D  /* 'dtim' */
-        case DeviceSettingsType             = 0x64657673  /* 'devs' Removed in V4 */
-        case Lut16Type                      = 0x6d667432  /* 'mft2' */
-        case Lut8Type                       = 0x6d667431  /* 'mft1' */
-        case LutAtoBType                    = 0x6d414220  /* 'mAB ' */
-        case LutBtoAType                    = 0x6d424120  /* 'mBA ' */
-        case MeasurementType                = 0x6D656173  /* 'meas' */
+        case Chromaticity               = 0x6368726D  /* 'chrm' */
+        case ColorantOrder              = 0x636C726F  /* 'clro' */
+        case ColorantTable              = 0x636C7274  /* 'clrt' */
+        case CrdInfo                    = 0x63726469  /* 'crdi' Removed in V4 */
+        case Curve                      = 0x63757276  /* 'curv' */
+        case Data                       = 0x64617461  /* 'data' */
+        case Dict                       = 0x64696374  /* 'dict' */
+        case DateTime                   = 0x6474696D  /* 'dtim' */
+        case DeviceSettings             = 0x64657673  /* 'devs' Removed in V4 */
+        case Lut16                      = 0x6d667432  /* 'mft2' */
+        case Lut8                       = 0x6d667431  /* 'mft1' */
+        case LutAtoB                    = 0x6d414220  /* 'mAB ' */
+        case LutBtoA                    = 0x6d424120  /* 'mBA ' */
+        case Measurement                = 0x6D656173  /* 'meas' */
         case MultiLocalizedUnicodeType      = 0x6D6C7563  /* 'mluc' */
         case MultiProcessElementType        = 0x6D706574  /* 'mpet' */
-        case NamedColor2Type                = 0x6E636C32  /* 'ncl2' */
-        case ParametricCurveType            = 0x70617261  /* 'para' */
+        case NamedColor2                = 0x6E636C32  /* 'ncl2' */
+        case ParametricCurve            = 0x70617261  /* 'para' */
         case ProfileSequenceDescType        = 0x70736571  /* 'pseq' */
-        case ProfileSequceIdType            = 0x70736964  /* 'psid' */
-        case ResponseCurveSet16Type         = 0x72637332  /* 'rcs2' */
-        case S15Fixed16ArrayType            = 0x73663332  /* 'sf32' */
-        case ScreeningType                  = 0x7363726E  /* 'scrn' Removed in V4 */
-        case SignatureType                  = 0x73696720  /* 'sig ' */
-        case TextType                       = 0x74657874  /* 'text' */
-        case TextDescriptionType            = 0x64657363  /* 'desc' Removed in V4 */
-        case U16Fixed16ArrayType            = 0x75663332  /* 'uf32' */
-        case UcrBgType                      = 0x62666420  /* 'bfd ' Removed in V4 */
-        case UInt16ArrayType                = 0x75693136  /* 'ui16' */
-        case UInt32ArrayType                = 0x75693332  /* 'ui32' */
-        case UInt64ArrayType                = 0x75693634  /* 'ui64' */
-        case UInt8ArrayType                 = 0x75693038  /* 'ui08' */
-        case ViewingConditionsType          = 0x76696577  /* 'view' */
-        case XYZArrayType                   = 0x58595A20  /* 'XYZ ' */
+        case ProfileSequceId            = 0x70736964  /* 'psid' */
+        case ResponseCurveSet16         = 0x72637332  /* 'rcs2' */
+        case S15Fixed16Array            = 0x73663332  /* 'sf32' */
+        case Screening                  = 0x7363726E  /* 'scrn' Removed in V4 */
+        case Signature                  = 0x73696720  /* 'sig ' */
+        case Text                       = 0x74657874  /* 'text' */
+        case TextDescription            = 0x64657363  /* 'desc' Removed in V4 */
+        case U16Fixed16Array            = 0x75663332  /* 'uf32' */
+        case UcrBg                      = 0x62666420  /* 'bfd ' Removed in V4 */
+        case UInt16Array                = 0x75693136  /* 'ui16' */
+        case UInt32Array                = 0x75693332  /* 'ui32' */
+        case UInt64Array                = 0x75693634  /* 'ui64' */
+        case UInt8Array                 = 0x75693038  /* 'ui08' */
+        case ViewingConditions          = 0x76696577  /* 'view' */
+        case XYZArray                   = 0x58595A20  /* 'XYZ ' */
     }
 }
