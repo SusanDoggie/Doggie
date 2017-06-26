c505fd12636cfc92373e6dac69392c5c719afe80
Susan <CloudDesign@Cloud-Design-IT.local>
2017-06-26 10:54:06 +0800

    clean

--- Playground.playground/Pages/Color.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Color.xcplaygroundpage/Contents.swift
@@ -7,8 +7,6 @@ let D50 = ColorSpace.cieXYZ(white: Point(x: 0.34567, y: 0.35850))
 
 ColorSpace.adobeRGB.convert(RGBColorModel(red: 0.5, green: 0, blue: 0), to: D50)
 
-ColorSpace.adobeRGB.normalized.convert(RGBColorModel(red: 0.5, green: 0, blue: 0), to: D50)
-
 ColorSpace.adobeRGB.convert(RGBColorModel(red: 0.5, green: 0, blue: 0), to: ColorSpace.adobeRGB.linearTone)
 
 ColorSpace.adobeRGB.linearTone.convert(RGBColorModel(red: 0.217755528144395, green: 0, blue: 0), to: ColorSpace.adobeRGB)
