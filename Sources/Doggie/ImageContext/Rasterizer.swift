f0eff5867cac898c32ad0f907aeab8c608ac0353
Susan <CloudDesign@Cloud-Design-IT.local>
2017-07-04 15:14:46 +0800

    simplify

--- Sources/Doggie/ImageContext/Rasterizer.swift
+++ Sources/Doggie/ImageContext/Rasterizer.swift
@@ -87,87 +87,62 @@ extension RasterizeBufferProtocol {
             return (q * y + r, q)
         }
         
-        let d = cross(p1 - p0, p2 - p0)
-        
-        if !d.almostZero() {
+        @inline(__always)
+        func intRange(_ min: Double, _ max: Double, _ bound: CountableRange<Int>) -> CountableRange<Int> {
             
-            var q0 = p0
-            var q1 = p1
-            var q2 = p2
+            let _min = min.rounded(.up)
+            let _max = max.rounded(.down)
             
-            sort(&q0, &q1, &q2) { $0.y < $1.y }
+            let __min = Int(_min)
+            let __max = Int(_max)
             
-            let y0 = Int(q0.y.rounded().clamped(to: 0...Double(height - 1)))
-            let y1 = Int(q1.y.rounded().clamped(to: 0...Double(height - 1)))
-            let y2 = Int(q2.y.rounded().clamped(to: 0...Double(height - 1)))
+            guard __min <= __max else { return (__min..<__min).clamped(to: bound) }
             
-            var buf = self
-            
-            if let (mid_x, _) = scan(q0, q2, q1.y) {
-                
-                buf += y0 * width
-                
-                @inline(__always)
-                func _drawLoop(_ range: CountableClosedRange<Int>, _ x0: Double, _ dx0: Double, _ x1: Double, _ dx1: Double, operation: (Point, Self) throws -> Void) rethrows {
-                    
-                    let (min_x, min_dx, max_x, max_dx) = mid_x < q1.x ? (x0, dx0, x1, dx1) : (x1, dx1, x0, dx0)
-                    
-                    var _min_x = min_x
-                    var _max_x = max_x
-                    
-                    for y in range {
-                        let _y = Double(y)
-                        if _min_x < _max_x && q0.y..<q2.y ~= _y {
-                            let __min_x = Int(_min_x.rounded().clamped(to: 0...Double(width - 1)))
-                            let __max_x = Int(_max_x.rounded().clamped(to: 0...Double(width - 1)))
-                            var pixel = buf + __min_x
-                            for x in __min_x...__max_x {
-                                let _x = Double(x)
-                                if _min_x..<_max_x ~= _x {
-                                    try operation(Point(x: _x, y: _y), pixel)
-                                }
-                                pixel += 1
-                            }
-                        }
-                        _min_x += min_dx
-                        _max_x += max_dx
-                        buf += width
-                    }
-                }
-                
-                if q1.y < Double(y1) {
-                    
-                    if let (x0, dx0) = scan(q0, q2, Double(y1)), let (x2, dx2) = scan(q1, q2, Double(y1)) {
-                        
-                        if y0 < y1, let (x0, dx0) = scan(q0, q2, Double(y0)), let (x1, dx1) = scan(q0, q1, Double(y0)) {
-                            
-                            try _drawLoop(y0...y1 - 1, x0, dx0, x1, dx1, operation: operation)
-                        }
-                        
-                        try _drawLoop(y1...y2, x0, dx0, x2, dx2, operation: operation)
-                        
-                    } else if let (x0, dx0) = scan(q0, q2, Double(y0)), let (x1, dx1) = scan(q0, q1, Double(y0)) {
-                        
-                        try _drawLoop(y0...y1, x0, dx0, x1, dx1, operation: operation)
-                    }
-                } else {
-                    
-                    if let (x0, dx0) = scan(q0, q2, Double(y0)), let (x1, dx1) = scan(q0, q1, Double(y0)) {
-                        
-                        try _drawLoop(y0...y1, x0, dx0, x1, dx1, operation: operation)
-                        
-                        if y1 < y2, let (x0, dx0) = scan(q0, q2, Double(y1)), let (x2, dx2) = scan(q1, q2, Double(y1)) {
-                            
-                            try _drawLoop(y1 + 1...y2, x0 + dx0, dx0, x2 + dx2, dx2, operation: operation)
-                        }
-                    } else if let (x0, dx0) = scan(q0, q2, Double(y1)), let (x2, dx2) = scan(q1, q2, Double(y1)) {
-                        
-                        try _drawLoop(y1...y2, x0, dx0, x2, dx2, operation: operation)
-                    }
-                }
-            }
-            
+            return _max == max ? (__min..<__max).clamped(to: bound) : CountableRange(__min...__max).clamped(to: bound)
         }
+        
+        guard !cross(p1 - p0, p2 - p0).almostZero() else { return }
+        
+        var q0 = p0
+        var q1 = p1
+        var q2 = p2
+        
+        sort(&q0, &q1, &q2) { $0.y < $1.y }
+        
+        guard let (mid_x, _) = scan(q0, q2, q1.y) else { return }
+        
+        @inline(__always)
+        func _drawLoop(_ s0: Point, _ s1: Point, operation: (Point, Self) throws -> Void) rethrows {
+            
+            let y_range = intRange(s0.y, s1.y, 0..<height)
+            
+            var buf = self + y_range.lowerBound * width
+            
+            guard let (x0, dx0) = scan(q0, q2, Double(y_range.lowerBound)) else { return }
+            guard let (x1, dx1) = scan(s0, s1, Double(y_range.lowerBound)) else { return }
+            
+            let (min_x, min_dx, max_x, max_dx) = mid_x < q1.x ? (x0, dx0, x1, dx1) : (x1, dx1, x0, dx0)
+            
+            var _min_x = min_x
+            var _max_x = max_x
+            
+            for y in y_range {
+                if _min_x < _max_x {
+                    let x_range = intRange(_min_x, _max_x, 0..<width)
+                    var pixel = buf + x_range.lowerBound
+                    for x in x_range {
+                        try operation(Point(x: x, y: y), pixel)
+                        pixel += 1
+                    }
+                }
+                _min_x += min_dx
+                _max_x += max_dx
+                buf += width
+            }
+        }
+        
+        try _drawLoop(q0, q1, operation: operation)
+        try _drawLoop(q1, q2, operation: operation)
     }
     
 }
