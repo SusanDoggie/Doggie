bb55c3b1ffbd42d9cf1df683190a0c88a9f34b93
Susan <CloudDesign@Cloud-Design-IT.local>
2017-08-12 11:46:11 +0800

    clean

--- Playground.playground/Pages/Resampling.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Resampling.xcplaygroundpage/Contents.swift
@@ -5,30 +5,30 @@ import Doggie
 
 let sample = sampleImage(width: 100, height: 100)
 
-if let image = resampling(image: sample, width: 1000, height: 1000, resampling: .none, antialias: false).cgImage {
+if let image = resampling(image: sample, width: 500, height: 500, resampling: .none, antialias: false).cgImage {
     NSImage(cgImage: image)
 }
 
-if let image = resampling(image: sample, width: 1000, height: 1000, resampling: .linear, antialias: false).cgImage {
+if let image = resampling(image: sample, width: 500, height: 500, resampling: .linear, antialias: false).cgImage {
     NSImage(cgImage: image)
 }
 
-if let image = resampling(image: sample, width: 1000, height: 1000, resampling: .cosine, antialias: false).cgImage {
+if let image = resampling(image: sample, width: 500, height: 500, resampling: .cosine, antialias: false).cgImage {
     NSImage(cgImage: image)
 }
 
-if let image = resampling(image: sample, width: 1000, height: 1000, resampling: .cubic, antialias: false).cgImage {
+if let image = resampling(image: sample, width: 500, height: 500, resampling: .cubic, antialias: false).cgImage {
     NSImage(cgImage: image)
 }
 
-if let image = resampling(image: sample, width: 1000, height: 1000, resampling: .hermite(0.5, 0), antialias: false).cgImage {
+if let image = resampling(image: sample, width: 500, height: 500, resampling: .hermite(0.5, 0), antialias: false).cgImage {
     NSImage(cgImage: image)
 }
 
-if let image = resampling(image: sample, width: 1000, height: 1000, resampling: .mitchell(1/3, 1/3), antialias: false).cgImage {
+if let image = resampling(image: sample, width: 500, height: 500, resampling: .mitchell(1/3, 1/3), antialias: false).cgImage {
     NSImage(cgImage: image)
 }
 
-if let image = resampling(image: sample, width: 1000, height: 1000, resampling: .lanczos(3), antialias: false).cgImage {
+if let image = resampling(image: sample, width: 500, height: 500, resampling: .lanczos(3), antialias: false).cgImage {
     NSImage(cgImage: image)
 }
