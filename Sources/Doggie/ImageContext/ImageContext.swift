550769ff4713a3ca741c305082c0406352cb9dc6
Susan <CloudDesign@Cloud-Design-IT.local>
2017-08-21 14:13:18 +0800

    clean

--- Sources/Doggie/ImageContext/ImageContext.swift
+++ Sources/Doggie/ImageContext/ImageContext.swift
@@ -26,55 +26,31 @@
 @_fixed_layout
 public class ImageContext<Pixel: ColorPixelProtocol> {
     
-    @_versioned
-    var _image: Image<Pixel>
+    public private(set) var image: Image<Pixel>
     
-    @_versioned
-    var clip: [Double]
+    private var clip: [Double]
+    private var depth: [Double]
     
-    @_versioned
-    var depth: [Double]
+    private var _opacity: Double = 1
+    private var _antialias: Bool = true
+    private var _transform: SDTransform = SDTransform.identity
+    private var _blendMode: ColorBlendMode = .default
+    private var _compositingMode: ColorCompositingMode = .default
+    private var _resamplingAlgorithm: ResamplingAlgorithm = .default
+    private var _renderCullingMode: ImageContextRenderCullMode = .none
+    private var _renderDepthCompareMode: ImageContextRenderDepthCompareMode = .always
+    private var _renderingIntent: RenderingIntent = .default
     
-    @_versioned
-    var _opacity: Double = 1
+    private var next: ImageContext?
     
-    @_versioned
-    var _antialias: Bool = true
-    
-    @_versioned
-    var _transform: SDTransform = SDTransform.identity
-    
-    @_versioned
-    var _blendMode: ColorBlendMode = .default
-    
-    @_versioned
-    var _compositingMode: ColorCompositingMode = .default
-    
-    @_versioned
-    var _resamplingAlgorithm: ResamplingAlgorithm = .default
-    
-    @_versioned
-    var _renderCullingMode: ImageContextRenderCullMode = .none
-    
-    @_versioned
-    var _renderDepthCompareMode: ImageContextRenderDepthCompareMode = .always
-    
-    @_versioned
-    var _renderingIntent: RenderingIntent = .default
-    
-    @_versioned
-    var next: ImageContext?
-    
-    @_inlineable
     public init(image: Image<Pixel>) {
-        self._image = image
+        self.image = image
         self.clip = [Double](repeating: 1, count: image.width * image.height)
         self.depth = [Double](repeating: 1, count: image.width * image.height)
     }
     
-    @_inlineable
     public init(width: Int, height: Int, resolution: Resolution = Resolution(resolution: 1, unit: .point), colorSpace: ColorSpace<Pixel.Model>) {
-        self._image = Image(width: width, height: height, resolution: resolution, colorSpace: colorSpace)
+        self.image = Image(width: width, height: height, resolution: resolution, colorSpace: colorSpace)
         self.clip = [Double](repeating: 1, count: width * height)
         self.depth = [Double](repeating: 1, count: width * height)
     }
@@ -82,8 +58,6 @@ public class ImageContext<Pixel: ColorPixelProtocol> {
 
 extension ImageContext {
     
-    @_versioned
-    @_inlineable
     convenience init<P>(copyStates context: ImageContext<P>, colorSpace: ColorSpace<Pixel.Model>) {
         self.init(width: context.width, height: context.height, colorSpace: colorSpace)
         self._antialias = context.antialias
@@ -94,36 +68,33 @@ extension ImageContext {
         self._renderCullingMode = context.renderCullingMode
         self._renderDepthCompareMode = context.renderDepthCompareMode
         self._renderingIntent = context.renderingIntent
-        self._image.colorSpace.chromaticAdaptationAlgorithm = context.colorSpace.chromaticAdaptationAlgorithm
+        self.image.colorSpace.chromaticAdaptationAlgorithm = context.colorSpace.chromaticAdaptationAlgorithm
     }
 }
 
 extension ImageContext {
     
-    @_inlineable
     public func withUnsafeMutableImageBufferPointer<R>(_ body: (inout UnsafeMutableBufferPointer<Pixel>) throws -> R) rethrows -> R {
         
         if let next = self.next {
             return try next.withUnsafeMutableImageBufferPointer(body)
         } else {
-            return try _image.withUnsafeMutableBufferPointer(body)
+            return try image.withUnsafeMutableBufferPointer(body)
         }
     }
     
-    @_inlineable
     public func withUnsafeImageBufferPointer<R>(_ body: (UnsafeBufferPointer<Pixel>) throws -> R) rethrows -> R {
         
         if let next = self.next {
             return try next.withUnsafeImageBufferPointer(body)
         } else {
-            return try _image.withUnsafeBufferPointer(body)
+            return try image.withUnsafeBufferPointer(body)
         }
     }
 }
 
 extension ImageContext {
     
-    @_inlineable
     public func withUnsafeMutableClipBufferPointer<R>(_ body: (inout UnsafeMutableBufferPointer<Double>) throws -> R) rethrows -> R {
         
         if let next = self.next {
@@ -133,7 +104,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public func withUnsafeClipBufferPointer<R>(_ body: (UnsafeBufferPointer<Double>) throws -> R) rethrows -> R {
         
         if let next = self.next {
@@ -143,7 +113,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public func clearClipBuffer(with value: Double = 1) {
         
         withUnsafeMutableClipBufferPointer { buf in
@@ -161,7 +130,6 @@ extension ImageContext {
 
 extension ImageContext {
     
-    @_inlineable
     public var opacity: Double {
         get {
             return next?.opacity ?? _opacity
@@ -175,7 +143,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public var antialias: Bool {
         get {
             return next?.antialias ?? _antialias
@@ -189,7 +156,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public var transform: SDTransform {
         get {
             return next?.transform ?? _transform
@@ -203,7 +169,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public var blendMode: ColorBlendMode {
         get {
             return next?.blendMode ?? _blendMode
@@ -217,7 +182,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public var compositingMode: ColorCompositingMode {
         get {
             return next?.compositingMode ?? _compositingMode
@@ -231,7 +195,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public var resamplingAlgorithm: ResamplingAlgorithm {
         get {
             return next?.resamplingAlgorithm ?? _resamplingAlgorithm
@@ -245,7 +208,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public var renderingIntent: RenderingIntent {
         get {
             return next?.renderingIntent ?? _renderingIntent
@@ -259,16 +221,15 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public var chromaticAdaptationAlgorithm: ChromaticAdaptationAlgorithm {
         get {
-            return next?.chromaticAdaptationAlgorithm ?? _image.colorSpace.chromaticAdaptationAlgorithm
+            return next?.chromaticAdaptationAlgorithm ?? image.colorSpace.chromaticAdaptationAlgorithm
         }
         set {
             if let next = self.next {
                 next.chromaticAdaptationAlgorithm = newValue
             } else {
-                _image.colorSpace.chromaticAdaptationAlgorithm = newValue
+                image.colorSpace.chromaticAdaptationAlgorithm = newValue
             }
         }
     }
@@ -295,7 +256,6 @@ public enum ImageContextRenderDepthCompareMode {
 
 extension ImageContext {
     
-    @_inlineable
     public func withUnsafeMutableDepthBufferPointer<R>(_ body: (inout UnsafeMutableBufferPointer<Double>) throws -> R) rethrows -> R {
         
         if let next = self.next {
@@ -305,7 +265,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public func withUnsafeDepthBufferPointer<R>(_ body: (UnsafeBufferPointer<Double>) throws -> R) rethrows -> R {
         
         if let next = self.next {
@@ -318,7 +277,6 @@ extension ImageContext {
 
 extension ImageContext {
     
-    @_inlineable
     public var renderCullingMode: ImageContextRenderCullMode {
         get {
             return next?.renderCullingMode ?? _renderCullingMode
@@ -332,7 +290,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public var renderDepthCompareMode: ImageContextRenderDepthCompareMode {
         get {
             return next?.renderDepthCompareMode ?? _renderDepthCompareMode
@@ -346,7 +303,6 @@ extension ImageContext {
         }
     }
     
-    @_inlineable
     public func clearRenderDepthBuffer(with value: Double = 1) {
         
         withUnsafeMutableDepthBufferPointer { buf in
@@ -364,28 +320,107 @@ extension ImageContext {
 
 extension ImageContext {
     
-    @_inlineable
     public var colorSpace: ColorSpace<Pixel.Model> {
-        return next?.colorSpace ?? _image.colorSpace
+        return next?.colorSpace ?? image.colorSpace
     }
     
     @_inlineable
     public var width: Int {
-        return _image.width
+        return image.width
     }
     
     @_inlineable
     public var height: Int {
-        return _image.height
+        return image.height
     }
     
     @_inlineable
     public var resolution: Resolution {
-        return _image.resolution
+        return image.resolution
+    }
+}
+
+extension ImageContext {
+    
+    public func beginTransparencyLayer() {
+        
+        if let next = self.next {
+            next.beginTransparencyLayer()
+        } else {
+            
+            let width = self.width
+            let height = self.height
+            
+            if width == 0 || height == 0 {
+                return
+            }
+            
+            self.next = ImageContext(copyStates: self, colorSpace: colorSpace)
+        }
     }
     
-    @_inlineable
-    public var image: Image<Pixel> {
-        return _image
+    public func endTransparencyLayer() {
+        
+        if let next = self.next {
+            
+            if next.next != nil {
+                
+                next.endTransparencyLayer()
+                
+            } else {
+                
+                let width = self.width
+                let height = self.height
+                
+                self.next = nil
+                
+                if width == 0 || height == 0 {
+                    return
+                }
+                
+                next.image.withUnsafeBufferPointer { source in
+                    
+                    if var _source = source.baseAddress {
+                        
+                        self.withUnsafePixelBlender { blender in
+                            
+                            var blender = blender
+                            
+                            for _ in 0..<width * height {
+                                
+                                blender.draw { _source.pointee }
+                                
+                                blender += 1
+                                _source += 1
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension ImageContext {
+    
+    public func drawClip<P>(body: (ImageContext<P>) throws -> Void) rethrows where P.Model == GrayColorModel {
+        
+        if let next = self.next {
+            try next.drawClip(body: body)
+            return
+        }
+        
+        let width = self.width
+        let height = self.height
+        
+        if width == 0 || height == 0 {
+            return
+        }
+        
+        let _clip = ImageContext<P>(copyStates: self, colorSpace: ColorSpace.calibratedGray(from: colorSpace))
+        
+        try body(_clip)
+        
+        self.clip = _clip.image.pixels.map { $0.color.white * $0.opacity }
     }
 }
