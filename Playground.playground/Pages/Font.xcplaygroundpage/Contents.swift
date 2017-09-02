f163939f12337727a7e203d18d32cf0e76517421
Susan Cheng <Susan@SusandeMac-mini.local>
2017-09-02 22:37:55 +0800

    playground test

---
+++ Playground.playground/Pages/Font.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import Doggie
+
+let data = try Data(contentsOf: URL(fileURLWithPath: "/Library/Fonts/Arial.ttf"))
+
+let collection = try FontCollection(data: data)
+
+if let font = collection.first?.with(size: 14) {
+    
+    font.fontName
+    font.displayName
+    font.uniqueName
+    font.familyName
+    font.faceName
+    
+    font.designer
+    
+    font.version
+    
+    font.trademark
+    font.manufacturer
+    font.license
+    font.copyright
+    
+    font.ascender
+    font.descender
+    font.lineGap
+    
+    font.verticalAscender
+    font.verticalDescender
+    font.verticalLineGap
+    
+    font.unitsPerEm
+    
+    font.boundingRectForFont
+    
+    font.italicAngle
+    font.isFixedPitch
+    
+    font.underlinePosition
+    font.underlineThickness
+    
+    let glyph = font.glyph(with: "a")
+    
+    font.boundary(forGlyph: glyph)
+    
+    font.shape(forGlyph: glyph)
+    
+}
