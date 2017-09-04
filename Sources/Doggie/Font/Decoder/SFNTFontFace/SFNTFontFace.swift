9cf2a6257691d99afe07c133eaa82620d1fe6cba
Susan <CloudDesign@Cloud-Design-IT.local>
2017-09-04 15:27:13 +0800

    clean

--- Sources/Doggie/Font/Decoder/SFNTFontFace/SFNTFontFace.swift
+++ Sources/Doggie/Font/Decoder/SFNTFontFace/SFNTFontFace.swift
@@ -39,8 +39,7 @@ struct SFNTFontFace : FontFaceBase {
     var hmtx: Data
     var vhea: SFNTVHEA?
     var vmtx: Data?
-    var loca: Data?
-    var glyf: Data?
+    var glyf: SFNTGLYF?
     var cff: CFFFontFace?
     var cff2: CFF2Decoder?
     
@@ -92,12 +91,7 @@ struct SFNTFontFace : FontFaceBase {
             
         } else if let loca = table["loca"], let glyf = table["glyf"] {
             
-            let locaSize = head.indexToLocFormat == 0 ? (Int(maxp.numGlyphs) + 1) << 1 : (Int(maxp.numGlyphs) + 1) << 2
-            
-            guard loca.count >= locaSize else { throw DataDecodeError.endOfData }
-            
-            self.loca = loca
-            self.glyf = glyf
+            self.glyf = try SFNTGLYF(format: Int(head.indexToLocFormat), numGlyphs: Int(maxp.numGlyphs), loca: loca, glyf: glyf)
             
         } else {
             throw FontCollection.Error.InvalidFormat("outlines not found.")
@@ -118,249 +112,23 @@ extension SFNTFontFace {
 
 extension SFNTFontFace {
     
-    private func _glyfData(glyph: Int) -> Data? {
-        precondition(glyph < numberOfGlyphs, "Index out of range.")
-        guard let loca = self.loca, let glyf = self.glyf else { return nil }
-        let startIndex: Int
-        let endIndex: Int
-        if head.indexToLocFormat == 0 {
-            startIndex = Int(loca.withUnsafeBytes { $0[glyph] as BEUInt16 }) << 1
-            endIndex = Int(loca.withUnsafeBytes { $0[glyph + 1] as BEUInt16 }) << 1
-        } else {
-            startIndex = Int(loca.withUnsafeBytes { $0[glyph] as BEUInt32 })
-            endIndex = Int(loca.withUnsafeBytes { $0[glyph + 1] as BEUInt32 })
-        }
-        return glyf.dropFirst(startIndex).prefix(endIndex - startIndex)
-    }
-    
     func boundary(glyph: Int) -> Rect {
         
-        if var data = _glyfData(glyph: glyph) {
-            
-            guard let _ = try? data.decode(BEInt16.self) else { return Rect() }
-            guard let xMin = try? data.decode(BEInt16.self) else { return Rect() }
-            guard let yMin = try? data.decode(BEInt16.self) else { return Rect() }
-            guard let xMax = try? data.decode(BEInt16.self) else { return Rect() }
-            guard let yMax = try? data.decode(BEInt16.self) else { return Rect() }
-            
-            let minX = Double(xMin.representingValue)
-            let minY = Double(yMin.representingValue)
-            let maxX = Double(xMax.representingValue)
-            let maxY = Double(yMax.representingValue)
-            return Rect(x: minX, y: minY, width: maxX - minX, height: maxY - minY)
+        precondition(glyph < numberOfGlyphs, "Index out of range.")
+        
+        if let boundary = glyf?.boundary(glyph: glyph) {
+            return boundary
         }
         
         return Rect()
     }
     
-    private func _glyfOutline(_ glyph: Int) -> ([Point], Shape.Component)? {
-        
-        guard var data = _glyfData(glyph: glyph) else { return nil }
-        
-        guard let numberOfContours = try? data.decode(BEInt16.self), numberOfContours > 0 else { return nil }
-        guard let _ = try? data.decode(BEInt16.self) else { return nil }
-        guard let _ = try? data.decode(BEInt16.self) else { return nil }
-        guard let _ = try? data.decode(BEInt16.self) else { return nil }
-        guard let _ = try? data.decode(BEInt16.self) else { return nil }
-        
-        guard let endPtsOfContours = try? (0..<Int(numberOfContours)).map({ _ in try data.decode(BEUInt16.self) }) else { return nil }
-        
-        guard let instructionLength = try? data.decode(BEUInt16.self) else { return nil }
-        
-        guard let instructions = try? (0..<Int(instructionLength)).map({ _ in try data.decode(UInt8.self) }) else { return nil }
-        
-        let numberOfCoordinates = Int(endPtsOfContours.last!) + 1
-        
-        var flags = [UInt8]()
-        flags.reserveCapacity(numberOfCoordinates)
-        while flags.count < numberOfCoordinates {
-            guard let flag = try? data.decode(UInt8.self) else { return nil }
-            flags.append(flag)
-            if flag & 8 != 0 {
-                guard let _repeat = try? data.decode(UInt8.self) else { return nil }
-                for _ in 0..<_repeat {
-                    flags.append(flag)
-                }
-            }
-        }
-        guard flags.count == numberOfCoordinates else { return nil }
-        
-        func coordinate(_ flag: UInt8, _ previousValue: Int16, _ bitMask: (UInt8, UInt8)) throws -> Int16 {
-            var code: Int16
-            if flag & bitMask.0 != 0 {
-                code = Int16(try data.decode(Int8.self))
-                if flag & bitMask.1 == 0 {
-                    code = -code
-                }
-                code = previousValue + code
-            } else {
-                if flag & bitMask.1 != 0 {
-                    code = previousValue
-                } else {
-                    code = previousValue + Int16(try data.decode(BEInt16.self))
-                }
-            }
-            return code
-        }
-        
-        var x_coordinate: [Int16] = []
-        var y_coordinate: [Int16] = []
-        x_coordinate.reserveCapacity(numberOfCoordinates)
-        y_coordinate.reserveCapacity(numberOfCoordinates)
-        
-        for flag in flags {
-            guard let _x = try? coordinate(flag, x_coordinate.last ?? 0, (2, 16)) else { return nil }
-            x_coordinate.append(_x)
-        }
-        
-        for flag in flags {
-            guard let _y = try? coordinate(flag, y_coordinate.last ?? 0, (4, 32)) else { return nil }
-            y_coordinate.append(_y)
-        }
-        
-        let points = zip(x_coordinate, y_coordinate).map { Point(x: Double($0), y: Double($1)) }
-        
-        var component = Shape.Component(start: Point(), closed: true, segments: [])
-        
-        if flags[numberOfCoordinates - 1] & 1 == 1 {
-            component.start = points[numberOfCoordinates - 1]
-        } else if flags[0] & 1 == 1 {
-            component.start = points[0]
-        } else {
-            component.start = 0.5 * (points[numberOfCoordinates - 1] + points[0])
-        }
-        
-        var record = (flags[numberOfCoordinates - 1], points[numberOfCoordinates - 1])
-        
-        for (f, p) in zip(zip(flags, flags.rotated(1)), zip(points, points.rotated(1))) {
-            
-            if f.0 & 1 == 0 {
-                if f.1 & 1 == 1 {
-                    component.append(.quad(p.0, p.1))
-                } else {
-                    component.append(.quad(p.0, 0.5 * (p.0 + p.1)))
-                }
-            } else {
-                if record.0 & 1 == 0 {
-                    component.append(.quad(0.5 * (p.0 + record.1), p.0))
-                } else {
-                    component.append(.line(p.0))
-                }
-            }
-            
-            record = (f.0, p.0)
-        }
-        
-        return (points, component)
-    }
-    
     func shape(glyph: Int) -> [Shape.Component] {
         
-        if var data = _glyfData(glyph: glyph) {
-            
-            guard let numberOfContours = try? BEInt16(data), numberOfContours != 0 else { return [] }
-            
-            if numberOfContours > 0 {
-                
-                print("data:", data)
-                print("numberOfContours:", numberOfContours)
-                
-                return self._glyfOutline(glyph).map { [$0.1] } ?? []
-                
-            } else {
-                
-                var components = [Shape.Component]()
-                
-                var _continue = true
-                
-                var points = [Point]()
-                
-                while _continue {
-                    
-                    guard let flags = try? data.decode(BEUInt16.self) else { return [] }
-                    guard let glyphIndex = try? data.decode(BEUInt16.self), glyphIndex != glyph else { return [] }
-                    
-                    guard let (_points, component) = self._glyfOutline(Int(glyphIndex)) else { return [] }
-                    
-                    var transform = SDTransform.identity
-                    
-                    if flags & 1 != 0 {
-                        
-                        if flags & 2 != 0 {
-                            
-                            guard let dx = try? data.decode(BEInt16.self) else { return [] }
-                            guard let dy = try? data.decode(BEInt16.self) else { return [] }
-                            
-                            transform.c = Double(dx.representingValue)
-                            transform.f = Double(dy.representingValue)
-                            
-                        } else {
-                            
-                            guard let m0 = try? data.decode(BEUInt16.self), m0 < points.count else { return [] }
-                            guard let m1 = try? data.decode(BEUInt16.self), m1 < _points.count else { return [] }
-                            
-                            let offset = points[Int(m0)] - _points[Int(m1)]
-                            
-                            transform.c = offset.x
-                            transform.f = offset.y
-                        }
-                    } else {
-                        
-                        if flags & 2 != 0 {
-                            
-                            guard let dx = try? data.decode(Int8.self) else { return [] }
-                            guard let dy = try? data.decode(Int8.self) else { return [] }
-                            
-                            transform.c = Double(dx)
-                            transform.f = Double(dy)
-                            
-                        } else {
-                            
-                            guard let m0 = try? data.decode(UInt8.self), m0 < points.count else { return [] }
-                            guard let m1 = try? data.decode(UInt8.self), m1 < _points.count else { return [] }
-                            
-                            let offset = points[Int(m0)] - _points[Int(m1)]
-                            
-                            transform.c = offset.x
-                            transform.f = offset.y
-                        }
-                    }
-                    
-                    if flags & 8 != 0 {
-                        
-                        guard let scale = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
-                        
-                        transform.a = scale.representingValue
-                        transform.e = scale.representingValue
-                        
-                    } else if flags & 64 != 0 {
-                        
-                        guard let x_scale = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
-                        guard let y_scale = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
-                        
-                        transform.a = x_scale.representingValue
-                        transform.e = y_scale.representingValue
-                        
-                    } else if flags & 128 != 0 {
-                        
-                        guard let m00 = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
-                        guard let m01 = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
-                        guard let m10 = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
-                        guard let m11 = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
-                        
-                        transform.a = m00.representingValue
-                        transform.b = m01.representingValue
-                        transform.d = m10.representingValue
-                        transform.e = m11.representingValue
-                    }
-                    
-                    points.append(contentsOf: _points)
-                    components.append(component * transform)
-                    _continue = flags & 32 != 0
-                }
-                
-                return components
-            }
+        precondition(glyph < numberOfGlyphs, "Index out of range.")
+        
+        if let shape = glyf?.shape(glyph: glyph) {
+            return shape
         }
         
         return []
---
+++ Sources/Doggie/Font/Decoder/SFNTFontFace/SFNTGLYF.swift
@@ -0,0 +1,304 @@
+//
+//  SFNTGLYF.swift
+//
+//  The MIT License
+//  Copyright (c) 2015 - 2017 Susan Cheng. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+
+struct SFNTGLYF {
+    
+    var format: Int
+    var loca: Data
+    var glyf: Data
+    
+    init(format: Int, numGlyphs: Int, loca: Data, glyf: Data) throws {
+        let locaSize = format == 0 ? (numGlyphs + 1) << 1 : (numGlyphs + 1) << 2
+        guard loca.count >= locaSize else { throw DataDecodeError.endOfData }
+        self.format = format
+        self.loca = loca
+        self.glyf = glyf
+    }
+    
+}
+
+extension SFNTGLYF {
+    
+    private func _glyfData(glyph: Int) -> Data {
+        
+        let startIndex: Int
+        let endIndex: Int
+        
+        if format == 0 {
+            startIndex = Int(loca.withUnsafeBytes { $0[glyph] as BEUInt16 }) << 1
+            endIndex = Int(loca.withUnsafeBytes { $0[glyph + 1] as BEUInt16 }) << 1
+        } else {
+            startIndex = Int(loca.withUnsafeBytes { $0[glyph] as BEUInt32 })
+            endIndex = Int(loca.withUnsafeBytes { $0[glyph + 1] as BEUInt32 })
+        }
+        
+        return glyf.dropFirst(startIndex).prefix(endIndex - startIndex)
+    }
+    
+    func boundary(glyph: Int) -> Rect {
+        
+        var data = _glyfData(glyph: glyph)
+        
+        guard let _ = try? data.decode(BEInt16.self) else { return Rect() }
+        guard let xMin = try? data.decode(BEInt16.self) else { return Rect() }
+        guard let yMin = try? data.decode(BEInt16.self) else { return Rect() }
+        guard let xMax = try? data.decode(BEInt16.self) else { return Rect() }
+        guard let yMax = try? data.decode(BEInt16.self) else { return Rect() }
+        
+        let minX = Double(xMin.representingValue)
+        let minY = Double(yMin.representingValue)
+        let maxX = Double(xMax.representingValue)
+        let maxY = Double(yMax.representingValue)
+        
+        return Rect(x: minX, y: minY, width: maxX - minX, height: maxY - minY)
+    }
+    
+    private func _glyfOutline(_ glyph: Int) -> ([Point], [Shape.Component])? {
+        
+        var data = _glyfData(glyph: glyph)
+        
+        guard let numberOfContours = try? data.decode(BEInt16.self), numberOfContours > 0 else { return nil }
+        guard let _ = try? data.decode(BEInt16.self) else { return nil }
+        guard let _ = try? data.decode(BEInt16.self) else { return nil }
+        guard let _ = try? data.decode(BEInt16.self) else { return nil }
+        guard let _ = try? data.decode(BEInt16.self) else { return nil }
+        
+        guard let endPtsOfContours = try? (0..<Int(numberOfContours)).map({ _ in try data.decode(BEUInt16.self) }) else { return nil }
+        
+        guard let instructionLength = try? data.decode(BEUInt16.self) else { return nil }
+        
+        guard let instructions = try? (0..<Int(instructionLength)).map({ _ in try data.decode(UInt8.self) }) else { return nil }
+        
+        let numberOfCoordinates = Int(endPtsOfContours.last!) + 1
+        
+        var flags = [UInt8]()
+        flags.reserveCapacity(numberOfCoordinates)
+        while flags.count < numberOfCoordinates {
+            guard let flag = try? data.decode(UInt8.self) else { return nil }
+            flags.append(flag)
+            if flag & 8 != 0 {
+                guard let _repeat = try? data.decode(UInt8.self) else { return nil }
+                for _ in 0..<_repeat {
+                    flags.append(flag)
+                }
+            }
+        }
+        guard flags.count == numberOfCoordinates else { return nil }
+        
+        func coordinate(_ flag: UInt8, _ previousValue: Int16, _ bitMask: (UInt8, UInt8)) throws -> Int16 {
+            let code: Int16
+            if flag & bitMask.0 != 0 {
+                if flag & bitMask.1 == 0 {
+                    code = previousValue - Int16(try data.decode(UInt8.self))
+                } else {
+                    code = previousValue + Int16(try data.decode(UInt8.self))
+                }
+            } else {
+                if flag & bitMask.1 != 0 {
+                    code = previousValue
+                } else {
+                    code = previousValue + Int16(try data.decode(BEInt16.self))
+                }
+            }
+            return code
+        }
+        
+        var x_coordinate: [Int16] = []
+        var y_coordinate: [Int16] = []
+        x_coordinate.reserveCapacity(numberOfCoordinates)
+        y_coordinate.reserveCapacity(numberOfCoordinates)
+        
+        for flag in flags {
+            guard let _x = try? coordinate(flag, x_coordinate.last ?? 0, (2, 16)) else { return nil }
+            x_coordinate.append(_x)
+        }
+        
+        for flag in flags {
+            guard let _y = try? coordinate(flag, y_coordinate.last ?? 0, (4, 32)) else { return nil }
+            y_coordinate.append(_y)
+        }
+        
+        let points = zip(x_coordinate, y_coordinate).map { Point(x: Double($0), y: Double($1)) }
+        
+        var components: [Shape.Component] = []
+        
+        var startIndex = 0
+        for _endIndex in endPtsOfContours {
+            
+            let endIndex = Int(_endIndex)
+            
+            guard startIndex < endIndex else { return nil }
+            
+            let range = startIndex...endIndex
+            let flags = flags[range]
+            let points = points[range]
+            
+            var component = Shape.Component(start: Point(), closed: true, segments: [])
+            
+            if flags[numberOfCoordinates - 1] & 1 == 1 {
+                component.start = points[endIndex]
+            } else if flags[0] & 1 == 1 {
+                component.start = points[startIndex]
+            } else {
+                component.start = 0.5 * (points[endIndex] + points[startIndex])
+            }
+            
+            var record = (flags[endIndex], points[endIndex])
+            
+            for (f, p) in zip(zip(flags, flags.rotated(1)), zip(points, points.rotated(1))) {
+                
+                if f.0 & 1 == 0 {
+                    if f.1 & 1 == 1 {
+                        component.append(.quad(p.0, p.1))
+                    } else {
+                        component.append(.quad(p.0, 0.5 * (p.0 + p.1)))
+                    }
+                } else {
+                    if record.0 & 1 == 0 {
+                        component.append(.quad(0.5 * (p.0 + record.1), p.0))
+                    } else {
+                        component.append(.line(p.0))
+                    }
+                }
+                
+                record = (f.0, p.0)
+            }
+            
+            components.append(component)
+            
+            startIndex = endIndex + 1
+        }
+        
+        return (points, components)
+    }
+    
+    func shape(glyph: Int) -> [Shape.Component] {
+        
+        var data = _glyfData(glyph: glyph)
+        
+        guard let numberOfContours = try? BEInt16(data), numberOfContours != 0 else { return [] }
+        
+        if numberOfContours > 0 {
+            
+            return self._glyfOutline(glyph).map { $0.1 } ?? []
+            
+        } else {
+            
+            var components = [Shape.Component]()
+            
+            var _continue = true
+            
+            var points = [Point]()
+            
+            while _continue {
+                
+                guard let flags = try? data.decode(BEUInt16.self) else { return [] }
+                guard let glyphIndex = try? data.decode(BEUInt16.self), glyphIndex != glyph else { return [] }
+                
+                guard let (_points, _components) = self._glyfOutline(Int(glyphIndex)) else { return [] }
+                
+                var transform = SDTransform.identity
+                
+                if flags & 1 != 0 {
+                    
+                    if flags & 2 != 0 {
+                        
+                        guard let dx = try? data.decode(BEInt16.self) else { return [] }
+                        guard let dy = try? data.decode(BEInt16.self) else { return [] }
+                        
+                        transform.c = Double(dx.representingValue)
+                        transform.f = Double(dy.representingValue)
+                        
+                    } else {
+                        
+                        guard let m0 = try? data.decode(BEUInt16.self), m0 < points.count else { return [] }
+                        guard let m1 = try? data.decode(BEUInt16.self), m1 < _points.count else { return [] }
+                        
+                        let offset = points[Int(m0)] - _points[Int(m1)]
+                        
+                        transform.c = offset.x
+                        transform.f = offset.y
+                    }
+                } else {
+                    
+                    if flags & 2 != 0 {
+                        
+                        guard let dx = try? data.decode(Int8.self) else { return [] }
+                        guard let dy = try? data.decode(Int8.self) else { return [] }
+                        
+                        transform.c = Double(dx)
+                        transform.f = Double(dy)
+                        
+                    } else {
+                        
+                        guard let m0 = try? data.decode(UInt8.self), m0 < points.count else { return [] }
+                        guard let m1 = try? data.decode(UInt8.self), m1 < _points.count else { return [] }
+                        
+                        let offset = points[Int(m0)] - _points[Int(m1)]
+                        
+                        transform.c = offset.x
+                        transform.f = offset.y
+                    }
+                }
+                
+                if flags & 8 != 0 {
+                    
+                    guard let scale = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
+                    
+                    transform.a = scale.representingValue
+                    transform.e = scale.representingValue
+                    
+                } else if flags & 64 != 0 {
+                    
+                    guard let x_scale = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
+                    guard let y_scale = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
+                    
+                    transform.a = x_scale.representingValue
+                    transform.e = y_scale.representingValue
+                    
+                } else if flags & 128 != 0 {
+                    
+                    guard let m00 = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
+                    guard let m01 = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
+                    guard let m10 = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
+                    guard let m11 = try? data.decode(Fixed14Number<BEInt16>.self) else { return [] }
+                    
+                    transform.a = m00.representingValue
+                    transform.b = m01.representingValue
+                    transform.d = m10.representingValue
+                    transform.e = m11.representingValue
+                }
+                
+                points.append(contentsOf: _points)
+                components.append(contentsOf: _components.map { $0 * transform })
+                _continue = flags & 32 != 0
+            }
+            
+            return components
+        }
+    }
+}
