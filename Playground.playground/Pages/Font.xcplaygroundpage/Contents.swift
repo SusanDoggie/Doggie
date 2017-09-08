fcccf08bbfa2e99b92ac1e4360bafc0e8f782b32
Susan <CloudDesign@Cloud-Design-IT.local>
2017-09-08 18:44:07 +0800

    clean

--- Playground.playground/Pages/Font.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Font.xcplaygroundpage/Contents.swift
@@ -3,11 +3,9 @@
 import Cocoa
 import Doggie
 
-let data = try Data(contentsOf: URL(fileURLWithPath: "/Library/Fonts/Arial.ttf"))
+let Arial = try FontCollection(data: try Data(contentsOf: URL(fileURLWithPath: "/Library/Fonts/Arial.ttf")))
 
-let collection = try FontCollection(data: data)
-
-if let font = collection.first?.with(size: 64) {
+if let font = Arial.first?.with(size: 64) {
     
     let string = "Doggie\u{0301}".precomposedStringWithCompatibilityMapping
     
@@ -24,3 +22,24 @@ if let font = collection.first?.with(size: 64) {
     
     shape
 }
+
+let PingFang = try FontCollection(data: try Data(contentsOf: URL(fileURLWithPath: "/System/Library/Fonts/PingFang.ttc")))
+
+if let font = PingFang.first?.with(size: 64) {
+    
+    let string = "Doggie\u{0301}".precomposedStringWithCompatibilityMapping
+    
+    let glyphs = string.unicodeScalars.map { font.glyph(with: $0) }
+    let advances = glyphs.map { font.advanceWidth(forGlyph: $0) }.scan(0, +)
+    
+    var shape = Shape()
+    
+    for (advance, glyph) in zip(advances, glyphs) {
+        var outline = font.shape(forGlyph: glyph)
+        outline.center.x += advance
+        shape.append(contentsOf: outline.identity)
+    }
+    
+    shape
+}
+
