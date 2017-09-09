93a2acf27418bbd25ca369b0848b9ddd820ebc9d
Susan <CloudDesign@Cloud-Design-IT.local>
2017-09-09 11:53:15 +0800

    clean

--- Playground.playground/Pages/Font.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Font.xcplaygroundpage/Contents.swift
@@ -9,6 +9,28 @@ let availableFonts = FontCollection.availableFonts
 
 t.timeIntervalSinceNow
 
+let Arial = availableFonts.filter { $0.familyName == "Arial" }.sorted { $0.fontName }
+
+for font in Arial {
+    
+    let font = font.with(size: 64)
+    
+    let string = "Doggie\u{0301}".precomposedStringWithCompatibilityMapping
+    
+    let glyphs = string.unicodeScalars.map { font.glyph(with: $0) }
+    let advances = glyphs.map { font.advanceWidth(forGlyph: $0) }.scan(0, +)
+    
+    var shape = Shape()
+    
+    for (advance, glyph) in zip(advances, glyphs) {
+        var outline = font.shape(forGlyph: glyph)
+        outline.center.x += advance
+        shape.append(contentsOf: outline.identity)
+    }
+    
+    shape
+}
+
 let PingFang = availableFonts.filter { $0.familyName == "PingFang HK" }.sorted { $0.fontName }
 
 for font in PingFang {
