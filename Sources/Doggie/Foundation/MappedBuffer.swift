cc7b1049ad82626ce0ec51e3eb5781f2ad2fc654
Susan Cheng <Susan@SusandeMac-mini.local>
2017-09-26 22:08:02 +0800

    clean

--- Sources/Doggie/Foundation/MappedBuffer.swift
+++ Sources/Doggie/Foundation/MappedBuffer.swift
@@ -57,6 +57,13 @@ public struct MappedBuffer<Element> : RandomAccessCollection, MutableCollection,
     }
 }
 
+extension MappedBuffer {
+    
+    public var capacity: Int {
+        return buffer.capacity
+    }
+}
+
 extension MappedBuffer : CustomStringConvertible {
     
     @_inlineable
@@ -81,6 +88,7 @@ extension MappedBuffer {
         self.buffer = new_buffer
     }
     
+    @_inlineable
     public var startIndex: Int {
         return 0
     }
@@ -89,13 +97,14 @@ extension MappedBuffer {
         return buffer.count
     }
     
+    @_inlineable
     public subscript(position: Int) -> Element {
         get {
-            precondition(0..<buffer.count ~= position, "Index out of range.")
+            precondition(self.indices ~= position, "Index out of range.")
             return self.withUnsafeBufferPointer { $0[position] }
         }
         set {
-            precondition(0..<buffer.count ~= position, "Index out of range.")
+            precondition(self.indices ~= position, "Index out of range.")
             self.withUnsafeMutableBufferPointer { $0[position] = newValue }
         }
     }
@@ -105,18 +114,24 @@ extension MappedBuffer : RangeReplaceableCollection {
     
     public mutating func append(_ x: Element) {
         
-        if isKnownUniquelyReferenced(&buffer) && buffer.count + 1 <= buffer.capacity {
+        let new_count = buffer.count + 1
+        
+        if isKnownUniquelyReferenced(&buffer) {
+            
+            if buffer.capacity < new_count {
+                buffer.extend_capacity(capacity: new_count)
+            }
             
             let append = buffer.address + buffer.count
             append.initialize(to: x)
             
-            buffer.count += 1
+            buffer.count = new_count
             
         } else {
             
-            let new_buffer = MappedBufferTempFile<Element>(capacity: buffer.count + 1)
+            let new_buffer = MappedBufferTempFile<Element>(capacity: new_count)
             
-            new_buffer.count = buffer.count + 1
+            new_buffer.count = new_count
             new_buffer.address.initialize(from: buffer.address, count: buffer.count)
             
             let append = new_buffer.address + buffer.count
@@ -128,7 +143,13 @@ extension MappedBuffer : RangeReplaceableCollection {
     
     public mutating func reserveCapacity(_ minimumCapacity: Int) {
         
-        if buffer.capacity <= minimumCapacity {
+        guard buffer.capacity < minimumCapacity else { return }
+        
+        if isKnownUniquelyReferenced(&buffer) {
+            
+            buffer.extend_capacity(capacity: minimumCapacity)
+            
+        } else {
             
             let new_buffer = MappedBufferTempFile<Element>(capacity: minimumCapacity)
             
@@ -147,7 +168,11 @@ extension MappedBuffer : RangeReplaceableCollection {
         let newElements_count = Int(newElements.count)
         let new_count = buffer.count - subRange.count + newElements_count
         
-        if isKnownUniquelyReferenced(&buffer) && new_count <= buffer.capacity {
+        if isKnownUniquelyReferenced(&buffer) {
+            
+            if buffer.capacity < new_count {
+                buffer.extend_capacity(capacity: new_count)
+            }
             
             let destroy = buffer.address + subRange.lowerBound
             destroy.deinitialize(count: subRange.count)
@@ -249,20 +274,13 @@ extension MappedBuffer {
     }
 }
 
-extension MappedBuffer {
-    
-    public var capacity: Int {
-        return buffer.capacity
-    }
-}
-
 private class MappedBufferTempFile<Element> {
     
     let fd: Int32
-    let raw_address: UnsafeMutableRawPointer
-    let address: UnsafeMutablePointer<Element>
-    let mapped_size: Int
-    let capacity: Int
+    private(set) var raw_address: UnsafeMutableRawPointer
+    private(set) var address: UnsafeMutablePointer<Element>
+    private(set) var mapped_size: Int
+    private(set) var capacity: Int
     var count: Int = 0
     
     init(capacity: Int) {
@@ -289,6 +307,21 @@ private class MappedBufferTempFile<Element> {
         munmap(raw_address, mapped_size)
         ftruncate(fd, 0)
         close(fd)
+    }
+    
+    func extend_capacity(capacity: Int) {
+        
+        precondition(self.capacity <= capacity)
+        
+        munmap(raw_address, mapped_size)
+        
+        self.mapped_size = (max(capacity, 1) * MemoryLayout<Element>.stride).align(Int(PAGE_SIZE))
+        self.capacity = mapped_size / MemoryLayout<Element>.stride
+        
+        guard ftruncate(self.fd, off_t(mapped_size)) != -1 else { fatalError("\(String(cString: strerror(errno)))") }
+        
+        self.raw_address = mmap(nil, mapped_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)
+        self.address = raw_address.bindMemory(to: Element.self, capacity: capacity)
     }
 }
 
