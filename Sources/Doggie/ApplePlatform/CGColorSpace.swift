9462f02805ca1e0bf49ad8cb4d6f0d1f30abe0ad
SusanDoggie <susan.doggie@gmail.com>
2018-06-05 14:49:56 +0800

    merge

--- Sources/Doggie/ApplePlatform/CGColorSpace.swift
+++ Sources/Doggie/ApplePlatform/CGColorSpace.swift
@@ -39,25 +39,7 @@ extension ColorSpace {
                 
             } else {
                 
-                if Model.numberOfComponents != 1 && Model.numberOfComponents != 3 && Model.numberOfComponents != 4 {
-                    return nil
-                }
-                
-                if let iccData = self.iccData.flatMap({ CGDataProvider(data: $0 as CFData) }) {
-                    
-                    var range: [CGFloat] = []
-                    
-                    for i in 0..<Model.numberOfComponents {
-                        let _range = Model.rangeOfComponent(i)
-                        range.append(CGFloat(_range.lowerBound))
-                        range.append(CGFloat(_range.upperBound))
-                    }
-                    
-                    return CGColorSpace(iccBasedNComponents: Model.numberOfComponents, range: range, profile: iccData, alternate: nil)
-                    
-                } else {
-                    return nil
-                }
+                return self.iccData.flatMap { CGColorSpace(iccProfileData: $0 as CFData) }
             }
         }
     }
