62e8dab14c70e14dabc88116352b259cd073c534
SusanDoggie <susan.doggie@gmail.com>
2018-07-16 11:02:49 +0800

    DisplacementMap

---
+++ Sources/Doggie/ImageFilter/DisplacementMap.swift
@@ -0,0 +1,62 @@
+//
+//  DisplacementMap.swift
+//
+//  The MIT License
+//  Copyright (c) 2015 - 2018 Susan Cheng. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+@inlinable
+public func DisplacementMap<S, T>(_ source: Image<S>, _ displacement: Image<T>, _ xChannelSelector: Int, _ yChannelSelector: Int, _ scale: Double) -> Image<S> {
+    
+    let texture = Texture(image: source)
+    
+    let width = displacement.width
+    let height = displacement.height
+    
+    var result = Image<S>(width: width, height: height, colorSpace: source.colorSpace)
+    
+    result.withUnsafeMutableBufferPointer {
+        
+        guard var result = $0.baseAddress else { return }
+        
+        displacement.withUnsafeBufferPointer {
+            
+            guard var displacement = $0.baseAddress else { return }
+            
+            for y in 0..<height {
+                for x in 0..<width {
+                    
+                    let d = displacement.pointee
+                    
+                    let _x = Double(x) + scale * (d.component(xChannelSelector) - 0.5)
+                    let _y = Double(y) + scale * (d.component(yChannelSelector) - 0.5)
+                    
+                    result.pointee = S(texture.pixel(Point(x: _x, y: _y)))
+                    
+                    displacement += 1
+                    result += 1
+                }
+            }
+        }
+    }
+    
+    return result
+}
