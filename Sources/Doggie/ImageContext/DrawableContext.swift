4743567ad8e84e62dad5a61c44da5013128a0a37
SusanDoggie <susan.doggie@gmail.com>
2018-10-30 17:09:54 +0800

    clean

--- Sources/Doggie/ImageContext/DrawableContext.swift
+++ Sources/Doggie/ImageContext/DrawableContext.swift
@@ -280,6 +280,179 @@ extension TypedDrawableContext {
     }
 }
 
+public enum GradientSpreadMode {
+    
+    case none
+    case pad
+    case reflect
+    case `repeat`
+}
+
+public struct GradientStop<Color: ColorProtocol> {
+    
+    public var offset: Double
+    public var color: Color
+    
+    @inlinable
+    public init(offset: Double, color: Color) {
+        self.offset = offset
+        self.color = color
+    }
+}
+
+extension GradientStop : Equatable where Color : Equatable {
+    
+    @inlinable
+    public static func ==(lhs: GradientStop, rhs: GradientStop) -> Bool {
+        return lhs.offset == rhs.offset && lhs.color == rhs.color
+    }
+}
+
+extension GradientStop : Hashable where Color : Hashable {
+    
+    @inlinable
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(offset)
+        hasher.combine(color)
+    }
+}
+
+extension GradientStop where Color == AnyColor {
+    
+    @inlinable
+    public init<M>(_ stop: GradientStop<Doggie.Color<M>>) {
+        self.init(offset: stop.offset, color: AnyColor(stop.color))
+    }
+    
+    @inlinable
+    public init<M>(offset: Double, color: Doggie.Color<M>) {
+        self.init(offset: offset, color: AnyColor(color))
+    }
+}
+
+public enum GradientType {
+    
+    case linear
+    case radial
+}
+
+public struct Gradient<Color: ColorProtocol> {
+    
+    public var type: GradientType
+    
+    public var start: Point
+    public var end: Point
+    
+    fileprivate var rawCenter: Point {
+        switch type {
+        case .linear: return 0.5 * (start + end)
+        case .radial: return end
+        }
+    }
+    
+    public var center: Point {
+        get {
+            return rawCenter * transform
+        }
+        set {
+            let _center = center
+            if _center != newValue {
+                let offset = newValue - rawCenter * transform
+                transform *= SDTransform.translate(x: offset.x, y: offset.y)
+            }
+        }
+    }
+    
+    public var transform : SDTransform = SDTransform.identity
+    
+    public var opacity: Double = 1
+    
+    public var stops: [GradientStop<Color>]
+    
+    public init(type: GradientType, start: Point, end: Point, stops: [GradientStop<Color>]) {
+        self.type = type
+        self.start = start
+        self.end = end
+        self.stops = stops
+    }
+}
+
+extension Gradient where Color == AnyColor {
+    
+    @inlinable
+    public init<M>(_ gradient: Gradient<Doggie.Color<M>>) {
+        self.init(type: gradient.type, start: gradient.start, end: gradient.end, stops: gradient.stops.map(GradientStop<AnyColor>.init))
+        self.transform = gradient.transform
+        self.opacity = gradient.opacity
+    }
+}
+
+extension Gradient {
+    
+    @inlinable
+    public mutating func rotate(_ angle: Double) {
+        let center = self.center
+        self.transform *= SDTransform.translate(x: -center.x, y: -center.y) * SDTransform.rotate(angle) * SDTransform.translate(x: center.x, y: center.y)
+    }
+    
+    @inlinable
+    public mutating func skewX(_ angle: Double) {
+        let center = self.center
+        self.transform *= SDTransform.translate(x: -center.x, y: -center.y) * SDTransform.skewX(angle) * SDTransform.translate(x: center.x, y: center.y)
+    }
+    
+    @inlinable
+    public mutating func skewY(_ angle: Double) {
+        let center = self.center
+        self.transform *= SDTransform.translate(x: -center.x, y: -center.y) * SDTransform.skewY(angle) * SDTransform.translate(x: center.x, y: center.y)
+    }
+    
+    @inlinable
+    public mutating func scale(_ scale: Double) {
+        let center = self.center
+        self.transform *= SDTransform.translate(x: -center.x, y: -center.y) * SDTransform.scale(scale) * SDTransform.translate(x: center.x, y: center.y)
+    }
+    
+    @inlinable
+    public mutating func scale(x: Double = 1, y: Double = 1) {
+        let center = self.center
+        self.transform *= SDTransform.translate(x: -center.x, y: -center.y) * SDTransform.scale(x: x, y: y) * SDTransform.translate(x: center.x, y: center.y)
+    }
+    
+    @inlinable
+    public mutating func translate(x: Double = 0, y: Double = 0) {
+        self.transform *= SDTransform.translate(x: x, y: y)
+    }
+    
+    @inlinable
+    public mutating func reflectX() {
+        self.transform *= SDTransform.reflectX(self.center.x)
+    }
+    
+    @inlinable
+    public mutating func reflectY() {
+        self.transform *= SDTransform.reflectY(self.center.y)
+    }
+    
+    @inlinable
+    public mutating func reflectX(_ x: Double) {
+        self.transform *= SDTransform.reflectX(x)
+    }
+    
+    @inlinable
+    public mutating func reflectY(_ y: Double) {
+        self.transform *= SDTransform.reflectY(y)
+    }
+}
+
+extension Gradient : Equatable where Color : Equatable {
+    
+}
+
+extension Gradient : Hashable where Color : Hashable {
+    
+}
+
 extension DrawableContext {
     
     @inlinable
