e27ce18cd041fa7cc7e1216f25426057cf245b50
Susan Cheng <susan.doggie@gmail.com>
2019-05-09 13:01:14 +0800

    Update .travis-build.sh

--- .travis-build.sh
+++ .travis-build.sh
@@ -9,7 +9,12 @@ fi
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   export DEBIAN_FRONTEND=noninteractive
-  apt-get update && apt-get install -y git sudo lsb-release wget libxml2 zlib1g-dev msttcorefonts
+  apt-get update && apt-get install -y git sudo lsb-release wget libxml2 zlib1g-dev fontconfig
+fi
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
+  apt-get install -y ttf-mscorefonts-installer
 fi
 
 git clone https://github.com/IBM-Swift/Package-Builder.git
