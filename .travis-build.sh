fef14a057df49fb88dbe60564487b9f1e54ce613
Susan Cheng <susan.doggie@gmail.com>
2019-05-09 12:27:29 +0800

    Update .travis-build.sh

--- .travis-build.sh
+++ .travis-build.sh
@@ -10,7 +10,7 @@ exit $?
 fi
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-apt-get update && apt-get install -y git sudo lsb-release wget libxml2 zlib1g-dev ttf-mscorefonts-installer
+apt-get update && apt-get install -y git sudo lsb-release wget libxml2 zlib1g-dev msttcorefonts
 fi
 
 git clone https://github.com/IBM-Swift/Package-Builder.git
