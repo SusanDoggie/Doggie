18defe5fc0ab8af2b5c9862a10516901c21d1267
Susan Cheng <susan.doggie@gmail.com>
2019-05-09 18:24:35 +0800

    Update .travis-build.sh

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 
-if [ -n "${DOCKER_IMAGE}" ]; then
-
+if [ -e ./Package-Builder/build-package.sh ]; then
   git clone https://github.com/SusanDoggie/Package-Builder.git
+fi
 
+if [ -n "${DOCKER_IMAGE}" ]; then
   docker pull ${DOCKER_IMAGE}
   docker run --env SWIFT_SNAPSHOT -v ${TRAVIS_BUILD_DIR}:${TRAVIS_BUILD_DIR} ${DOCKER_IMAGE} /bin/bash -c "cd $TRAVIS_BUILD_DIR && ./.travis-build.sh"
   exit $?
-  
 fi
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-
 cat <<"EOF" > ./.before-install-swift
 #!/bin/bash
 set -e
@@ -20,7 +19,6 @@ export DEBIAN_FRONTEND=noninteractive
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt-get install -y fontconfig ttf-mscorefonts-installer
 EOF
-
 fi
 
 while true; do echo "..."; sleep 60; done &
