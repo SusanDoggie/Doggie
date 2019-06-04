43b4d494d5d49f51091bc4473f80117445b23aed
SusanDoggie <susan.doggie@gmail.com>
2019-06-04 17:00:02 +0800

    clean

--- Sources/Doggie/Foundation/Parallel.swift
+++ Sources/Doggie/Foundation/Parallel.swift
@@ -47,6 +47,15 @@ extension RandomAccessCollection {
         DispatchQueue.concurrentPerform(iterations: self.count) { body(self[self.index(startIndex, offsetBy: $0)]) }
     }
     
+    /// Call `body` on each element in `self` in parallel
+    ///
+    /// - Note: You cannot use the `break` or `continue` statement to exit the
+    ///   current call of the `body` closure or skip subsequent calls.
+    /// - Note: Using the `return` statement in the `body` closure will only
+    ///   exit from the current call to `body`, not any outer scope, and won't
+    ///   skip subsequent calls.
+    ///
+    /// - Parameter threads: Number of threads at the same time.
     @inlinable
     @inline(__always)
     public func parallelEach(threads: Int, body: (Element) -> ()) {
@@ -61,9 +70,9 @@ extension RandomAccessCollection {
     /// to lowercase strings and then to count their characters.
     ///
     ///     let cast = ["Vivien", "Marlon", "Kim", "Karl"]
-    ///     let lowercaseNames = cast.map { $0.lowercaseString }
+    ///     let lowercaseNames = cast.parallelMap { $0.lowercaseString }
     ///     // 'lowercaseNames' == ["vivien", "marlon", "kim", "karl"]
-    ///     let letterCounts = cast.map { $0.count }
+    ///     let letterCounts = cast.parallelMap { $0.count }
     ///     // 'letterCounts' == [6, 6, 3, 4]
     ///
     /// - Parameter transform: A mapping closure. `transform` accepts an
@@ -73,7 +82,21 @@ extension RandomAccessCollection {
     ///   sequence.
     @inlinable
     public func parallelMap<T>(_ transform: (Element) -> T) -> [T] {
+        
         let count = self.count
+        
+        #if swift(>=5.1)
+        
+        return Array(unsafeUninitializedCapacity: count) { buffer, initializedCount in
+            guard let buffer = buffer.baseAddress else { return }
+            DispatchQueue.concurrentPerform(iterations: count) {
+                (buffer + $0).initialize(to: transform(self[self.index(startIndex, offsetBy: $0)]))
+            }
+            initializedCount = count
+        }
+        
+        #else
+        
         let buffer = UnsafeMutablePointer<T>.allocate(capacity: count)
         DispatchQueue.concurrentPerform(iterations: count) {
             (buffer + $0).initialize(to: transform(self[self.index(startIndex, offsetBy: $0)]))
@@ -82,11 +105,46 @@ extension RandomAccessCollection {
         buffer.deinitialize(count: count)
         buffer.deallocate()
         return Array(result)
+        
+        #endif
     }
     
+    /// Returns an array containing the results of mapping the given closure
+    /// over the sequence's elements. The elements of the result are computed
+    /// in parallel.
+    ///
+    /// In this example, `map` is used first to convert the names in the array
+    /// to lowercase strings and then to count their characters.
+    ///
+    ///     let cast = ["Vivien", "Marlon", "Kim", "Karl"]
+    ///     let lowercaseNames = cast.parallelMap { $0.lowercaseString }
+    ///     // 'lowercaseNames' == ["vivien", "marlon", "kim", "karl"]
+    ///     let letterCounts = cast.parallelMap { $0.count }
+    ///     // 'letterCounts' == [6, 6, 3, 4]
+    ///
+    /// - Parameter threads: Number of threads at the same time.
+    /// - Parameter transform: A mapping closure. `transform` accepts an
+    ///   element of this sequence as its parameter and returns a transformed
+    ///   value of the same or of a different type.
+    /// - Returns: An array containing the transformed elements of this
+    ///   sequence.
     @inlinable
     public func parallelMap<T>(threads: Int, _ transform: (Element) -> T) -> [T] {
+        
         let count = self.count
+        
+        #if swift(>=5.1)
+        
+        return Array(unsafeUninitializedCapacity: count) { buffer, initializedCount in
+            guard let buffer = buffer.baseAddress else { return }
+            DispatchQueue.concurrentPerform(iterations: count, threads: threads) {
+                (buffer + $0).initialize(to: transform(self[self.index(startIndex, offsetBy: $0)]))
+            }
+            initializedCount = count
+        }
+        
+        #else
+        
         let buffer = UnsafeMutablePointer<T>.allocate(capacity: count)
         DispatchQueue.concurrentPerform(iterations: count, threads: threads) {
             (buffer + $0).initialize(to: transform(self[self.index(startIndex, offsetBy: $0)]))
@@ -95,5 +153,7 @@ extension RandomAccessCollection {
         buffer.deinitialize(count: count)
         buffer.deallocate()
         return Array(result)
+        
+        #endif
     }
 }
