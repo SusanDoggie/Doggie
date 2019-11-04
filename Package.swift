9683d264fd5d6a0e15c56b9f2ff1af1723b979c2
Susan Cheng <>
2019-11-04 21:15:45 +0800

    clean

--- Package.swift
+++ Package.swift
@@ -28,9 +28,6 @@ import PackageDescription
 
 let package = Package(
     name: "Doggie",
-    platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
-    ],
     products: [
         .library(name: "Doggie", type: .dynamic, targets: ["Doggie"]),
         ],
