950fdc4c6b382796d89f0fc3cb81b72b65b73992
Susan Cheng <susan.doggie@gmail.com>
2020-06-03 20:16:46 +0800

    clean

--- Tests/DoggieTests/ImageTest.swift
+++ Tests/DoggieTests/ImageTest.swift
@@ -364,25 +364,6 @@ class ImageTest: XCTestCase {
         }
     }
     
-    func testMeshGradientPerformance() {
-        
-        self.measure() {
-            
-            let context = ImageContext<ARGB32ColorPixel>(width: 300, height: 300, colorSpace: ColorSpace.sRGB)
-            
-            let patch = CubicBezierPatch(coonsPatch: Point(x: 50, y: 50), Point(x: 100, y: 0), Point(x: 200, y: 100), Point(x: 250, y: 50),
-                                         Point(x: 100, y: 100), Point(x: 300, y: 100),
-                                         Point(x: 0, y: 200), Point(x: 200, y: 200),
-                                         Point(x: 50, y: 250), Point(x: 100, y: 200), Point(x: 200, y: 300), Point(x: 250, y: 250))
-            
-            context.drawGradient(patch, color: Color(colorSpace: context.colorSpace, color: RGBColorModel(red: 1, green: 0, blue: 0)),
-                                 Color(colorSpace: context.colorSpace, color: RGBColorModel(red: 0, green: 1, blue: 0)),
-                                 Color(colorSpace: context.colorSpace, color: RGBColorModel(red: 0, green: 0, blue: 1)),
-                                 Color(colorSpace: context.colorSpace, color: RGBColorModel(red: 1, green: 1, blue: 1)))
-            
-        }
-    }
-    
     func testOrthographicProjectPerformance() {
         
         self.measure() {
