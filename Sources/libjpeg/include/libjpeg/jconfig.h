69f9adba53f1ce1435af1a55fdf0f49ea0259774
Susan Cheng <susan.doggie@gmail.com>
2020-07-24 19:44:33 +0800

    update

--- Sources/libjpeg/include/libjpeg/jconfig.h
+++ Sources/libjpeg/include/libjpeg/jconfig.h
@@ -4,10 +4,10 @@
 #define JPEG_LIB_VERSION  62
 
 /* libjpeg-turbo version */
-#define LIBJPEG_TURBO_VERSION  2.0.5
+#define LIBJPEG_TURBO_VERSION  2.0.6
 
 /* libjpeg-turbo version in integer form */
-#define LIBJPEG_TURBO_VERSION_NUMBER  2000005
+#define LIBJPEG_TURBO_VERSION_NUMBER  2000006
 
 /* Support arithmetic encoding */
 #define C_ARITH_CODING_SUPPORTED 1
--- Sources/libjpeg/include/libjpeg/jconfigint.h
+++ Sources/libjpeg/include/libjpeg/jconfigint.h
@@ -1,5 +1,5 @@
 /* libjpeg-turbo build number */
-#define BUILD  "20200624"
+#define BUILD  "20200724"
 
 /* Compiler's inline keyword */
 #undef inline
@@ -14,7 +14,7 @@
 #define PACKAGE_NAME  "libjpeg-turbo"
 
 /* Version number of package */
-#define VERSION  "2.0.5"
+#define VERSION  "2.0.6"
 
 /* The size of `size_t', as computed by sizeof. */
 #define SIZEOF_SIZE_T  8
