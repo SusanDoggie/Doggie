0203b962eca08f101511ba49bbe9e66d371f84b3
Susan Cheng <susan.doggie@gmail.com>
2020-08-25 21:49:15 +0800

    clean

--- Sources/DoggieGraphics/ImageContext/DrawShape.swift
+++ Sources/DoggieGraphics/ImageContext/DrawShape.swift
@@ -40,7 +40,7 @@ extension ImageContext {
         
         let (bound, stencil) = self._stencil(shape: shape)
         
-        guard !bound.isNull else { return }
+        guard !bound.isEmpty else { return }
         
         guard var _min_x = Int(exactly: floor(bound.minX)) else { return }
         guard var _min_y = Int(exactly: floor(bound.minY)) else { return }
