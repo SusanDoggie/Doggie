687f87901374050fda1adfc30cd667bb8d870872
Susan Cheng <susan.doggie@gmail.com>
2020-08-25 20:16:49 +0800

    clean

--- Sources/DoggieGraphics/SVGContext/SVGEffect/SVGMergeEffect.swift
+++ Sources/DoggieGraphics/SVGContext/SVGEffect/SVGMergeEffect.swift
@@ -36,7 +36,7 @@ public struct SVGMergeEffect: SVGEffectElement {
     }
     
     public func visibleBound(_ sources: [SVGEffect.Source: Rect]) -> Rect? {
-        return self.sources.compactMap { sources[$0] }.reduce { lhs, rhs in lhs.union(rhs) }
+        return self.sources.lazy.map { sources[$0] }.reduce { lhs, rhs in rhs.flatMap { lhs?.union($0) } }.flatMap { $0 }
     }
 }
 
