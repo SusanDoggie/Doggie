59fecf54cda1de471abbb9ba734f265bc4dcadc8
Susan Cheng <susan.doggie@gmail.com>
2020-09-09 01:48:18 +0800

    clean

---
+++ Sources/DoggieGPU/CoreImage/SVGComponentTransferKernel.swift
@@ -0,0 +1,242 @@
+//
+//  SVGComponentTransferKernel.swift
+//
+//  The MIT License
+//  Copyright (c) 2015 - 2020 Susan Cheng. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#if canImport(CoreImage)
+
+extension CIImage {
+    
+    @available(macOS 10.13, *)
+    private class SVGComponentTransferKernel: CIImageProcessorKernel {
+        
+        enum TransferFunctionType: String, CaseIterable {
+            
+            case identity
+            case table
+            case discrete
+            case gamma
+        }
+        
+        static let function_constants: [String: MTLFunctionConstantValues] = {
+            
+            var function_constants: [String: MTLFunctionConstantValues] = [:]
+            
+            for alpha in TransferFunctionType.allCases {
+                
+                for blue in TransferFunctionType.allCases {
+                    
+                    for green in TransferFunctionType.allCases {
+                        
+                        for red in TransferFunctionType.allCases {
+                            
+                            let constants = MTLFunctionConstantValues()
+                            
+                            withUnsafeBytes(of: red == .table || red == .discrete) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "has_red_channel_table") }
+                            withUnsafeBytes(of: red == .discrete) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "is_red_channel_discrete") }
+                            withUnsafeBytes(of: red == .gamma) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "has_red_channel_gamma") }
+                            
+                            withUnsafeBytes(of: green == .table || green == .discrete) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "has_green_channel_table") }
+                            withUnsafeBytes(of: green == .discrete) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "is_green_channel_discrete") }
+                            withUnsafeBytes(of: green == .gamma) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "has_green_channel_gamma") }
+                            
+                            withUnsafeBytes(of: blue == .table || blue == .discrete) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "has_blue_channel_table") }
+                            withUnsafeBytes(of: blue == .discrete) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "is_blue_channel_discrete") }
+                            withUnsafeBytes(of: blue == .gamma) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "has_blue_channel_gamma") }
+                            
+                            withUnsafeBytes(of: alpha == .table || alpha == .discrete) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "has_alpha_channel_table") }
+                            withUnsafeBytes(of: alpha == .discrete) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "is_alpha_channel_discrete") }
+                            withUnsafeBytes(of: alpha == .gamma) { constants.setConstantValue($0.baseAddress!, type: .bool, withName: "has_alpha_channel_gamma") }
+                            
+                            function_constants["_\(red.rawValue)_\(green.rawValue)_\(blue.rawValue)_\(alpha.rawValue)"] = constants
+                        }
+                    }
+                }
+            }
+            
+            return function_constants
+        }()
+        
+        override class var synchronizeInputs: Bool {
+            return false
+        }
+        
+        override class func process(with inputs: [CIImageProcessorInput]?, arguments: [String: Any]?, output: CIImageProcessorOutput) throws {
+            
+            guard let commandBuffer = output.metalCommandBuffer else { return }
+            guard let source = inputs?[0].metalTexture else { return }
+            guard let output_texture = output.metalTexture else { return }
+            guard let red = arguments?["red"] as? SVGComponentTransferEffect.TransferFunction else { return }
+            guard let green = arguments?["green"] as? SVGComponentTransferEffect.TransferFunction else { return }
+            guard let blue = arguments?["blue"] as? SVGComponentTransferEffect.TransferFunction else { return }
+            guard let alpha = arguments?["alpha"] as? SVGComponentTransferEffect.TransferFunction else { return }
+            
+            let _red: String
+            switch red {
+            case .identity: _red = "_identity"
+            case .table: _red = "_table"
+            case .discrete: _red = "_discrete"
+            case .gamma: _red = "_gamma"
+            }
+            
+            let _green: String
+            switch green {
+            case .identity: _green = "_identity"
+            case .table: _green = "_table"
+            case .discrete: _green = "_discrete"
+            case .gamma: _green = "_gamma"
+            }
+            
+            let _blue: String
+            switch blue {
+            case .identity: _blue = "_identity"
+            case .table: _blue = "_table"
+            case .discrete: _blue = "_discrete"
+            case .gamma: _blue = "_gamma"
+            }
+            
+            let _alpha: String
+            switch alpha {
+            case .identity: _alpha = "_identity"
+            case .table: _alpha = "_table"
+            case .discrete: _alpha = "_discrete"
+            case .gamma: _alpha = "_gamma"
+            }
+            
+            guard let function_constant = function_constants["\(_red)\(_green)\(_blue)\(_alpha)"] else { return }
+            guard let pipeline = self.make_pipeline(commandBuffer.device, "svg_component_transfer", function_constant) else { return }
+            
+            guard let encoder = commandBuffer.makeComputeCommandEncoder() else { return }
+            
+            encoder.setComputePipelineState(pipeline)
+            
+            encoder.setTexture(source, index: 0)
+            encoder.setTexture(output_texture , index: 1)
+            
+            switch red {
+            case .identity: break
+            case let .table(table):
+                withUnsafeBytes(of: Int32(table.count)) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 2) }
+                table.map { Float($0) }.withUnsafeBytes { encoder.setBytes($0.baseAddress!, length: $0.count, index: 6) }
+            case let .discrete(table):
+                withUnsafeBytes(of: Int32(table.count)) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 2) }
+                table.map { Float($0) }.withUnsafeBytes { encoder.setBytes($0.baseAddress!, length: $0.count, index: 6) }
+            case let .gamma(amplitude, exponent, offset):
+                withUnsafeBytes(of: (Float(amplitude), Float(exponent), Float(offset))) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 2) }
+            }
+            
+            switch green {
+            case .identity: break
+            case let .table(table):
+                withUnsafeBytes(of: Int32(table.count)) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 3) }
+                table.map { Float($0) }.withUnsafeBytes { encoder.setBytes($0.baseAddress!, length: $0.count, index: 7) }
+            case let .discrete(table):
+                withUnsafeBytes(of: Int32(table.count)) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 3) }
+                table.map { Float($0) }.withUnsafeBytes { encoder.setBytes($0.baseAddress!, length: $0.count, index: 7) }
+            case let .gamma(amplitude, exponent, offset):
+                withUnsafeBytes(of: (Float(amplitude), Float(exponent), Float(offset))) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 3) }
+            }
+            
+            switch blue {
+            case .identity: break
+            case let .table(table):
+                withUnsafeBytes(of: Int32(table.count)) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 4) }
+                table.map { Float($0) }.withUnsafeBytes { encoder.setBytes($0.baseAddress!, length: $0.count, index: 8) }
+            case let .discrete(table):
+                withUnsafeBytes(of: Int32(table.count)) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 4) }
+                table.map { Float($0) }.withUnsafeBytes { encoder.setBytes($0.baseAddress!, length: $0.count, index: 8) }
+            case let .gamma(amplitude, exponent, offset):
+                withUnsafeBytes(of: (Float(amplitude), Float(exponent), Float(offset))) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 4) }
+            }
+            
+            switch alpha {
+            case .identity: break
+            case let .table(table):
+                withUnsafeBytes(of: Int32(table.count)) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 5) }
+                table.map { Float($0) }.withUnsafeBytes { encoder.setBytes($0.baseAddress!, length: $0.count, index: 9) }
+            case let .discrete(table):
+                withUnsafeBytes(of: Int32(table.count)) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 5) }
+                table.map { Float($0) }.withUnsafeBytes { encoder.setBytes($0.baseAddress!, length: $0.count, index: 9) }
+            case let .gamma(amplitude, exponent, offset):
+                withUnsafeBytes(of: (Float(amplitude), Float(exponent), Float(offset))) { encoder.setBytes($0.baseAddress!, length: $0.count, index: 5) }
+            }
+            
+            let group_width = max(1, pipeline.threadExecutionWidth)
+            let group_height = max(1, pipeline.maxTotalThreadsPerThreadgroup / group_width)
+            
+            let threadsPerThreadgroup = MTLSize(width: group_width, height: group_height, depth: 1)
+            let threadgroupsPerGrid = MTLSize(width: (output_texture.width + group_width - 1) / group_width, height: (output_texture.height + group_height - 1) / group_height, depth: 1)
+            
+            encoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
+            encoder.endEncoding()
+        }
+    }
+    
+    open func componentTransfer(red: SVGComponentTransferEffect.TransferFunction,
+                                green: SVGComponentTransferEffect.TransferFunction,
+                                blue: SVGComponentTransferEffect.TransferFunction,
+                                alpha: SVGComponentTransferEffect.TransferFunction) throws -> CIImage {
+        
+        if extent.isEmpty { return .empty() }
+        
+        if red == .identity && green == .identity && blue == .identity && alpha == .identity {
+            return self
+        }
+        
+        switch red {
+        case let .table(table): guard !table.isEmpty else  { return .empty() }
+        case let .discrete(table): guard !table.isEmpty else  { return .empty() }
+        default: break
+        }
+        
+        switch green {
+        case let .table(table): guard !table.isEmpty else  { return .empty() }
+        case let .discrete(table): guard !table.isEmpty else  { return .empty() }
+        default: break
+        }
+        
+        switch blue {
+        case let .table(table): guard !table.isEmpty else  { return .empty() }
+        case let .discrete(table): guard !table.isEmpty else  { return .empty() }
+        default: break
+        }
+        
+        switch alpha {
+        case let .table(table): guard !table.isEmpty else  { return .empty() }
+        case let .discrete(table): guard !table.isEmpty else  { return .empty() }
+        default: break
+        }
+        
+        let _extent = extent.isInfinite ? extent : extent.insetBy(dx: .random(in: -1..<0), dy: .random(in: -1..<0))
+        
+        var rendered = try SVGComponentTransferKernel.apply(withExtent: _extent, inputs: [self.unpremultiplyingAlpha()], arguments: ["red": red, "green": green, "blue": blue, "alpha": alpha])
+        
+        if !extent.isInfinite {
+            rendered = rendered.cropped(to: extent)
+        }
+        
+        return rendered.premultiplyingAlpha()
+    }
+}
+
+#endif
---
+++ Sources/DoggieGPU/CoreImage/Shaders/SVGComponentTransferKernel.metal
@@ -0,0 +1,137 @@
+//
+//  SVGComponentTransferKernel.metal
+//
+//  The MIT License
+//  Copyright (c) 2015 - 2020 Susan Cheng. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+constant bool has_red_channel_table [[function_constant(0)]];
+constant bool has_green_channel_table [[function_constant(1)]];
+constant bool has_blue_channel_table [[function_constant(2)]];
+constant bool has_alpha_channel_table [[function_constant(3)]];
+
+constant bool is_red_channel_discrete [[function_constant(4)]];
+constant bool is_green_channel_discrete [[function_constant(5)]];
+constant bool is_blue_channel_discrete [[function_constant(6)]];
+constant bool is_alpha_channel_discrete [[function_constant(7)]];
+
+constant bool has_red_channel_gamma [[function_constant(8)]];
+constant bool has_green_channel_gamma [[function_constant(9)]];
+constant bool has_blue_channel_gamma [[function_constant(10)]];
+constant bool has_alpha_channel_gamma [[function_constant(11)]];
+
+half table_lookup(half c, int table_size, constant float *table) {
+    
+    const int n = table_size - 1;
+    
+    const float _c = clamp((float)c, 0.0, 1.0) * (float)n;
+    const int k = (int)trunc(_c);
+    
+    if (k < n) {
+        return mix(table[k], table[k + 1], fract(_c));
+    } else {
+        return table[n];
+    }
+}
+
+half discrete_table_lookup(half c, int table_size, constant float *table) {
+    
+    const int n = table_size - 1;
+    
+    const float _c = clamp((float)c, 0.0, 1.0) * (float)n;
+    const int k = (int)trunc(_c);
+    
+    if (k < n) {
+        return table[k];
+    } else {
+        return table[n];
+    }
+}
+
+half gamma_transfer(half c, float amplitude, float exponent, float offset) {
+    return amplitude * pow((float)c, exponent) + offset;
+}
+
+kernel void svg_component_transfer(texture2d<half, access::read> input [[texture(0)]],
+                                   texture2d<half, access::write> output [[texture(1)]],
+                                   constant int &red_channel_table_size [[buffer(2), function_constant(has_red_channel_table)]],
+                                   constant int &green_channel_table_size [[buffer(3), function_constant(has_green_channel_table)]],
+                                   constant int &blue_channel_table_size [[buffer(4), function_constant(has_blue_channel_table)]],
+                                   constant int &alpha_channel_table_size [[buffer(5), function_constant(has_alpha_channel_table)]],
+                                   constant float *red_channel_table [[buffer(6), function_constant(has_red_channel_table)]],
+                                   constant float *green_channel_table [[buffer(7), function_constant(has_green_channel_table)]],
+                                   constant float *blue_channel_table [[buffer(8), function_constant(has_blue_channel_table)]],
+                                   constant float *alpha_channel_table [[buffer(9), function_constant(has_alpha_channel_table)]],
+                                   constant packed_float3 &red_channel_gamma [[buffer(2), function_constant(has_red_channel_gamma)]],
+                                   constant packed_float3 &green_channel_gamma [[buffer(3), function_constant(has_green_channel_gamma)]],
+                                   constant packed_float3 &blue_channel_gamma [[buffer(4), function_constant(has_blue_channel_gamma)]],
+                                   constant packed_float3 &alpha_channel_gamma [[buffer(5), function_constant(has_alpha_channel_gamma)]],
+                                   uint2 gid [[thread_position_in_grid]]) {
+    
+    if (gid.x >= output.get_width() || gid.y >= output.get_height()) { return; }
+    
+    half4 color = input.read(gid);
+    
+    if (has_red_channel_table) {
+        if (is_red_channel_discrete) {
+            color.r = discrete_table_lookup(color.r, red_channel_table_size, red_channel_table);
+        } else {
+            color.r = table_lookup(color.r, red_channel_table_size, red_channel_table);
+        }
+    } else if (has_red_channel_gamma) {
+        color.r = gamma_transfer(color.r, red_channel_gamma[0], red_channel_gamma[1], red_channel_gamma[2]);
+    }
+    
+    if (has_green_channel_table) {
+        if (is_green_channel_discrete) {
+            color.g = discrete_table_lookup(color.g, green_channel_table_size, green_channel_table);
+        } else {
+            color.g = table_lookup(color.g, green_channel_table_size, green_channel_table);
+        }
+    } else if (has_green_channel_gamma) {
+        color.g = gamma_transfer(color.g, green_channel_gamma[0], green_channel_gamma[1], green_channel_gamma[2]);
+    }
+    
+    if (has_blue_channel_table) {
+        if (is_blue_channel_discrete) {
+            color.b = discrete_table_lookup(color.b, blue_channel_table_size, blue_channel_table);
+        } else {
+            color.b = table_lookup(color.b, blue_channel_table_size, blue_channel_table);
+        }
+    } else if (has_blue_channel_gamma) {
+        color.b = gamma_transfer(color.b, blue_channel_gamma[0], blue_channel_gamma[1], blue_channel_gamma[2]);
+    }
+    
+    if (has_alpha_channel_table) {
+        if (is_alpha_channel_discrete) {
+            color.a = discrete_table_lookup(color.a, alpha_channel_table_size, alpha_channel_table);
+        } else {
+            color.a = table_lookup(color.a, alpha_channel_table_size, alpha_channel_table);
+        }
+    } else if (has_alpha_channel_gamma) {
+        color.a = gamma_transfer(color.a, alpha_channel_gamma[0], alpha_channel_gamma[1], alpha_channel_gamma[2]);
+    }
+    
+    output.write(color, gid);
+}
