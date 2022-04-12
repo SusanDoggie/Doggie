912e853a3752bd190756b5a4565e21d2e200f8c6
Susan Cheng <susan.doggie@gmail.com>
2022-04-12 18:57:23 +0800

    Update Sendable.swift

--- Sources/DoggieCore/Concurrency/Sendable.swift
+++ Sources/DoggieCore/Concurrency/Sendable.swift
@@ -26,6 +26,9 @@
 #if compiler(>=5.5.2) && canImport(_Concurrency)
 
 @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+extension Decimal: @unchecked Sendable { }
+
+@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 extension URL: @unchecked Sendable { }
 
 @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
