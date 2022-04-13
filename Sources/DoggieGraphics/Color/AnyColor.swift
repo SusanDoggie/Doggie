2024863edb1b04d62e2ab096ea2dbef22396f0c9
Susan Cheng <susan.doggie@gmail.com>
2022-04-13 10:56:01 +0800

    Update AnyColor.swift

--- Sources/DoggieGraphics/Color/AnyColor.swift
+++ Sources/DoggieGraphics/Color/AnyColor.swift
@@ -134,7 +134,7 @@ extension AnyColor {
     
     @inlinable
     public var colorSpace: AnyColorSpace {
-        return AnyColorSpace(_base.colorSpace)
+        return AnyColorSpace(base: _base.colorSpace)
     }
     
     @inlinable
